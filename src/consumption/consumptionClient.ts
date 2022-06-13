af81ae9cb3150232c0a30003592f2a0e2258bfd9
Félicien DIANOUX <felicien.dianoux@arrow.com>
2022-06-13 18:06:03 +0200

    feat(add-consumption): add consumption endpoint

---
+++ src/consumption/consumptionClient.ts
@@ -0,0 +1,37 @@
+import { AbstractClient, Parameters } from '../abstractClient';
+import { ConsumptionBI } from './entities/bi/consumptionBI';
+import { GetResult } from '../getResult';
+import { Consumption } from './entities/consumption/consumption';
+
+export class ConsumptionClient extends AbstractClient {
+  /**
+   * The base path of the API
+   */
+  protected basePath = '/consumption';
+
+  public async getMonthlyConsumption(
+    licenseReference: string,
+    parameters: Parameters,
+  ): Promise<GetResult<Consumption>> {
+    this.path = `/monthly/license/${licenseReference}`;
+
+    return new GetResult(Consumption, await this.get(parameters));
+  }
+
+  public async getDailyConsumption(
+    licenseReference: string,
+    parameters: Parameters,
+  ): Promise<GetResult<Consumption>> {
+    this.path = `/daily/license/${licenseReference}`;
+
+    return new GetResult(Consumption, await this.get(parameters));
+  }
+
+  public async getBIConsumption(
+    parameters: Parameters,
+  ): Promise<GetResult<ConsumptionBI>> {
+    this.path = '/bi/top/monthly';
+
+    return new GetResult(ConsumptionBI, await this.get(parameters));
+  }
+}
