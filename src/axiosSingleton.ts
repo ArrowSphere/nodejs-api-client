c1327bbfe4b95bca7e3d4dc12f30009faa0cfd59
Félicien DIANOUX <felicien.dianoux@arrow.com>
2022-12-01 17:14:18 +0100

    improve logs

---
+++ src/axiosSingleton.ts
@@ -0,0 +1,77 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { cloneDeep } from 'lodash';
+
+export class AxiosSingleton {
+  private static _axiosInstance: AxiosInstance;
+
+  public static getInstance(): AxiosInstance {
+    if (!AxiosSingleton._axiosInstance) {
+      AxiosSingleton._axiosInstance = axios.create();
+      AxiosSingleton._initializedRequestInterceptor();
+      AxiosSingleton._initializedResponseInterceptor();
+      AxiosSingleton._axiosInstance.defaults.validateStatus = null;
+    }
+
+    return AxiosSingleton._axiosInstance;
+  }
+
+  private static _initializedRequestInterceptor() {
+    this._axiosInstance.interceptors.request.use(this._handleRequest);
+  }
+
+  private static _initializedResponseInterceptor() {
+    this._axiosInstance.interceptors.response.use(this._handleResponse);
+  }
+
+  /**
+   * @param request - Axios Request
+   */
+  private static _handleRequest(
+    request: AxiosRequestConfig,
+  ): AxiosRequestConfig {
+    console.log('AXIOS - Request : ', AxiosSingleton.cleanRequestLog(request));
+
+    return request;
+  }
+
+  /**
+   * @param response - Axios Response
+   */
+  private static _handleResponse(response: AxiosResponse): AxiosResponse {
+    console.log(
+      'AXIOS - Response : ',
+      AxiosSingleton.cleanResponseLog(response),
+    );
+
+    return response;
+  }
+
+  /**
+   * @param request - Axios Request
+   */
+  private static cleanRequestLog(
+    request: AxiosRequestConfig,
+  ): AxiosRequestConfig {
+    const tempRequest = cloneDeep(request);
+
+    // coverage bug delete undefined element
+    /* istanbul ignore next */
+    delete tempRequest.headers?.apiKey;
+
+    return tempRequest;
+  }
+
+  /**
+   * @param response - Axios Response
+   */
+  private static cleanResponseLog(response: AxiosResponse): AxiosResponse {
+    const tempResponse = cloneDeep(response);
+
+    // coverage bug delete undefined element
+    /* istanbul ignore next */
+    delete tempResponse.config.headers?.apiKey;
+    delete tempResponse.request;
+
+    return tempResponse;
+  }
+}
--- src/security/standards/index.ts
+++ src/security/standards/index.ts
@@ -4,3 +4,4 @@ export * from './entities/checks/securityChecks';
 export * from './entities/checks/checks';
 export * from './entities/resources/securityResources';
 export * from './entities/resources/resources';
+export * from './standardsClient';
