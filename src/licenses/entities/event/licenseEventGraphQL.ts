696162a08e9dcbe29fb5db4a11a865139ef1d4e1
Wilfried EVIEUX <wilfried.evieux@arrow.com>
2023-03-14 09:22:13 +0100

    ARE-23990 - feat(license): add license event entity types

---
+++ src/licenses/entities/event/licenseEventGraphQL.ts
@@ -0,0 +1,32 @@
+import { LicenseEvent } from './licenseEvent';
+
+export interface LicenseEventPagination {
+  perPage: number;
+  currentPage: number;
+  totalPage: number;
+  total: number;
+  next: string;
+  previous: string;
+}
+
+export interface PaginatedLicenseEvents {
+  filters: LicenseEventFilters[];
+  pagination: LicenseEventPagination;
+  events: Array<{ event: LicenseEvent }>;
+}
+
+export interface LicenseEventFilters {
+  name: string;
+  values: LicenseEventFiltersValues[];
+}
+
+export interface LicenseEventFiltersValues {
+  value: string;
+  count: number;
+}
+
+export interface LicenseEventQueryResponse {
+  data: {
+    getEvents: PaginatedLicenseEvents;
+  };
+}
