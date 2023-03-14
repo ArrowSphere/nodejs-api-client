016adaa7d1756ab4ca492d98397e53582d2b0a15
Wilfried EVIEUX <wilfried.evieux@arrow.com>
2023-03-14 09:22:13 +0100

    ARE-23990 - feat(license): add license event entity types

---
+++ src/licenses/entities/event/licenseEvent.ts
@@ -0,0 +1,74 @@
+export enum LicenseEventActionType {
+  AUTO_RENEW_OFF = 'autoRenewOff',
+  AUTO_RENEW_ON = 'autoRenewOn',
+  CANCELLED = 'cancelled',
+  CREATION = 'creation',
+  DISABLED = 'disabled',
+  EXPIRED = 'expired',
+  QUANTITY_UPDATE = 'quantityUpdate',
+  REACTIVATED = 'reactivated',
+  RENEW = 'renew',
+  SUSPENDED = 'suspended',
+}
+
+export interface LicenseEventVendor {
+  name: string;
+  code: string;
+}
+
+export interface LicenseEventCustomer {
+  name: string;
+  ref: string;
+}
+
+export interface LicenseEventReseller {
+  name: string;
+  ref: string;
+}
+
+export interface LicenseEventPriceband {
+  vendorSku: string;
+  arrowsphereSku: string;
+}
+
+export interface LicenseEventOffer {
+  arrowsphereSku: string;
+  name: string;
+}
+
+export interface LicenseEventQuantity {
+  current: number;
+  requested: number;
+}
+
+export interface LicenseEventUser {
+  name: string;
+  email: string;
+}
+
+export interface LicenseEventDetails {
+  message: string;
+}
+
+export interface LicenseEvent {
+  id: number;
+  partnerRef: string;
+  orderRef: string;
+  createdAt: string;
+  customer: LicenseEventCustomer;
+  reseller: LicenseEventReseller;
+  marketplace: string;
+  friendlyName: string;
+  vendor: LicenseEventVendor;
+  priceband: LicenseEventPriceband;
+  offer: LicenseEventOffer;
+  uom: string;
+  periodicity: number;
+  term: number;
+  trial: boolean;
+  quantity: LicenseEventQuantity;
+  statusCode: string;
+  user: LicenseEventUser;
+  actionType: LicenseEventActionType;
+  eventDetails: LicenseEventDetails;
+}
