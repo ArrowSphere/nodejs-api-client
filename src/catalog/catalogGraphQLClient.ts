3af9733d41d2e648605432b8172830becc19c4f0
JPB <jean-philippe.blond@arrow.com>
2023-04-21 10:47:02 +0200

    feat(catalog-graphql-client): new client and new available types to request data

--- src/catalog/catalogGraphQLClient.ts
+++ src/catalog/catalogGraphQLClient.ts
@@ -1,6 +1,7 @@
 import { AbstractGraphQLClient } from '../abstractGraphQLClient';
 import { GetProductsType } from './types/catalogGraphQLTypes';
-// import { inspect } from 'util';
+import { CatalogQuery } from './types/catalogGraphQLQueries';
+import { PublicApiClientException } from '../exception';
 
 export class CatalogGraphQLClient extends AbstractGraphQLClient {
   /**
@@ -17,5 +18,26 @@ export class CatalogGraphQLClient extends AbstractGraphQLClient {
     this.path = this.GRAPHQL;
 
     return await this.post(request);
+  }
+
+  public async findByQuery(
+    query: CatalogQuery,
+  ): Promise<GetProductsType | null> {
+    const queryStr: string = this.stringifyQuery(query);
+
+    try {
+      return await this.find(queryStr);
+    } catch (error: any) {
+      const exception: PublicApiClientException = this.mapToPublicApiException(
+        error,
+      );
+
+      const { mustRetry } = await this.handleError(exception);
+      if (mustRetry) {
+        return await this.find(queryStr);
+      }
+    }
+
+    return null;
   }
 }
