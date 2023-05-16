cd3405acb4b80cd1152065ee653eee792950cc2f
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2023-05-16 10:18:54 +0200

    security score update types

--- src/securityScore/entities/getCustomerAccountData.ts
+++ src/securityScore/entities/getCustomerAccountData.ts
@@ -1,41 +1,44 @@
 import { AbstractEntity } from '../../abstractEntity';
 import {
-  CheckByStandardType,
+  ChecksAggType,
   FilterType,
-  ScoreResultType,
   MonthlyTrendAggType,
-  IssuesAggType,
-  SeveritiesAggType,
+  PaginationsType,
   PeriodsType,
-  StandardType,
+  ScoreResultType,
+  ScoresAggType,
+  SeveritiesAggType,
   StandardsAggType,
+  StandardWithCheckType,
 } from '../types/securityScoreGraphQLTypes';
 import { SecurityScoreQueries } from '../types/queryArguments';
 
 export enum GetCustomerAccountDataFields {
   COLUMN_AVG_CURRENT_SCORE = 'avgCurrentScore',
-  COLUMN_CHECKS = 'checks',
+  COLUMN_CHECKS_AGG = 'checksAgg',
   COLUMN_FILTERS = 'filters',
-  COLUMN_ISSUE_AGG = 'issueAgg',
   COLUMN_MONTHLY_TREND_AGG = 'monthlyTrendAgg',
+  COLUMN_PAGINATION = 'pagination',
   COLUMN_PERIOD = 'period',
-  COLUMN_RESULT = 'result',
-  COLUMN_SEVERITY_AGG = 'severityAgg',
+  COLUMN_RESULTS = 'results',
+  COLUMN_SCORES_AGG = 'scoresAgg',
+  COLUMN_SEVERITIES_AGG = 'severitiesAgg',
   COLUMN_STANDARDS_AGG = 'standardsAgg',
   COLUMN_STANDARDS = 'standards',
 }
 
 export type GetCustomerAccountDataType = {
   [GetCustomerAccountDataFields.COLUMN_AVG_CURRENT_SCORE]?: number;
-  [GetCustomerAccountDataFields.COLUMN_CHECKS]?: [CheckByStandardType];
+  [GetCustomerAccountDataFields.COLUMN_CHECKS_AGG]?: ChecksAggType;
   [GetCustomerAccountDataFields.COLUMN_FILTERS]?: [FilterType];
-  [GetCustomerAccountDataFields.COLUMN_ISSUE_AGG]?: IssuesAggType;
   [GetCustomerAccountDataFields.COLUMN_MONTHLY_TREND_AGG]?: MonthlyTrendAggType;
+  [GetCustomerAccountDataFields.COLUMN_PAGINATION]?: PaginationsType;
   [GetCustomerAccountDataFields.COLUMN_PERIOD]?: PeriodsType;
-  [GetCustomerAccountDataFields.COLUMN_RESULT]?: ScoreResultType;
-  [GetCustomerAccountDataFields.COLUMN_SEVERITY_AGG]?: SeveritiesAggType;
+  [GetCustomerAccountDataFields.COLUMN_RESULTS]?: [ScoreResultType];
+  [GetCustomerAccountDataFields.COLUMN_SCORES_AGG]?: ScoresAggType;
+  [GetCustomerAccountDataFields.COLUMN_SEVERITIES_AGG]?: SeveritiesAggType;
   [GetCustomerAccountDataFields.COLUMN_STANDARDS_AGG]?: StandardsAggType;
-  [GetCustomerAccountDataFields.COLUMN_STANDARDS]?: [StandardType];
+  [GetCustomerAccountDataFields.COLUMN_STANDARDS]?: [StandardWithCheckType];
 };
 
 export type GetCustomerAccountDataGraphQLResultType = {
@@ -44,15 +47,16 @@ export type GetCustomerAccountDataGraphQLResultType = {
 
 export class GetCustomerAccountData extends AbstractEntity<GetCustomerAccountDataType> {
   readonly #avgCurrentScore?: number;
-  readonly #checks?: [CheckByStandardType];
+  readonly #checksAgg?: ChecksAggType;
   readonly #filters?: [FilterType];
-  readonly #issueAgg?: IssuesAggType;
   readonly #monthlyTrendAgg?: MonthlyTrendAggType;
+  readonly #pagination?: PaginationsType;
   readonly #period?: PeriodsType;
-  readonly #result?: ScoreResultType;
-  readonly #severityAgg?: SeveritiesAggType;
+  readonly #results?: [ScoreResultType];
+  readonly #scoresAgg?: ScoresAggType;
+  readonly #severitiesAgg?: SeveritiesAggType;
   readonly #standardsAgg?: StandardsAggType;
-  readonly #standards?: [StandardType];
+  readonly #standards?: [StandardWithCheckType];
 
   public constructor(getCustomerAccountDataInput: GetCustomerAccountDataType) {
     super(getCustomerAccountDataInput);
@@ -61,25 +65,31 @@ export class GetCustomerAccountData extends AbstractEntity<GetCustomerAccountDat
       getCustomerAccountDataInput[
         GetCustomerAccountDataFields.COLUMN_AVG_CURRENT_SCORE
       ];
-    this.#checks =
-      getCustomerAccountDataInput[GetCustomerAccountDataFields.COLUMN_CHECKS];
+    this.#checksAgg =
+      getCustomerAccountDataInput[
+        GetCustomerAccountDataFields.COLUMN_CHECKS_AGG
+      ];
     this.#filters =
       getCustomerAccountDataInput[GetCustomerAccountDataFields.COLUMN_FILTERS];
-    this.#issueAgg =
-      getCustomerAccountDataInput[
-        GetCustomerAccountDataFields.COLUMN_ISSUE_AGG
-      ];
     this.#monthlyTrendAgg =
       getCustomerAccountDataInput[
         GetCustomerAccountDataFields.COLUMN_MONTHLY_TREND_AGG
       ];
+    this.#pagination =
+      getCustomerAccountDataInput[
+        GetCustomerAccountDataFields.COLUMN_PAGINATION
+      ];
     this.#period =
       getCustomerAccountDataInput[GetCustomerAccountDataFields.COLUMN_PERIOD];
-    this.#result =
-      getCustomerAccountDataInput[GetCustomerAccountDataFields.COLUMN_RESULT];
-    this.#severityAgg =
+    this.#results =
+      getCustomerAccountDataInput[GetCustomerAccountDataFields.COLUMN_RESULTS];
+    this.#scoresAgg =
       getCustomerAccountDataInput[
-        GetCustomerAccountDataFields.COLUMN_SEVERITY_AGG
+        GetCustomerAccountDataFields.COLUMN_SCORES_AGG
+      ];
+    this.#severitiesAgg =
+      getCustomerAccountDataInput[
+        GetCustomerAccountDataFields.COLUMN_SEVERITIES_AGG
       ];
     this.#standardsAgg =
       getCustomerAccountDataInput[
@@ -95,52 +105,61 @@ export class GetCustomerAccountData extends AbstractEntity<GetCustomerAccountDat
     return this.#avgCurrentScore;
   }
 
-  get checks(): [CheckByStandardType] | undefined {
-    return this.#checks;
+  get checksAgg(): ChecksAggType | undefined {
+    return this.#checksAgg;
   }
 
   get filters(): [FilterType] | undefined {
     return this.#filters;
   }
 
-  get issueAgg(): IssuesAggType | undefined {
-    return this.#issueAgg;
-  }
-
   get monthlyTrendAgg(): MonthlyTrendAggType | undefined {
     return this.#monthlyTrendAgg;
+  }
+
+  get pagination(): PaginationsType | undefined {
+    return this.#pagination;
   }
 
   get period(): PeriodsType | undefined {
     return this.#period;
   }
 
-  get result(): ScoreResultType | undefined {
-    return this.#result;
+  get scoresAgg(): ScoresAggType | undefined {
+    return this.#scoresAgg;
+  }
+
+  get results(): [ScoreResultType] | undefined {
+    return this.#results;
+  }
+
+  get severitiesAgg(): SeveritiesAggType | undefined {
+    return this.#severitiesAgg;
   }
 
   get standardsAgg(): StandardsAggType | undefined {
     return this.#standardsAgg;
   }
 
-  get standards(): [StandardType] | undefined {
+  get standards(): [StandardWithCheckType] | undefined {
     return this.#standards;
   }
 
   public toJSON(): GetCustomerAccountDataType {
     return {
       [GetCustomerAccountDataFields.COLUMN_AVG_CURRENT_SCORE]: this
-        .#avgCurrentScore,
-      [GetCustomerAccountDataFields.COLUMN_CHECKS]: this.#checks,
-      [GetCustomerAccountDataFields.COLUMN_FILTERS]: this.#filters,
-      [GetCustomerAccountDataFields.COLUMN_ISSUE_AGG]: this.#issueAgg,
+        .avgCurrentScore,
+      [GetCustomerAccountDataFields.COLUMN_CHECKS_AGG]: this.checksAgg,
+      [GetCustomerAccountDataFields.COLUMN_FILTERS]: this.filters,
       [GetCustomerAccountDataFields.COLUMN_MONTHLY_TREND_AGG]: this
-        .#monthlyTrendAgg,
-      [GetCustomerAccountDataFields.COLUMN_PERIOD]: this.#period,
-      [GetCustomerAccountDataFields.COLUMN_RESULT]: this.#result,
-      [GetCustomerAccountDataFields.COLUMN_SEVERITY_AGG]: this.#severityAgg,
-      [GetCustomerAccountDataFields.COLUMN_STANDARDS_AGG]: this.#standardsAgg,
-      [GetCustomerAccountDataFields.COLUMN_STANDARDS]: this.#standards,
+        .monthlyTrendAgg,
+      [GetCustomerAccountDataFields.COLUMN_PAGINATION]: this.pagination,
+      [GetCustomerAccountDataFields.COLUMN_PERIOD]: this.period,
+      [GetCustomerAccountDataFields.COLUMN_SCORES_AGG]: this.scoresAgg,
+      [GetCustomerAccountDataFields.COLUMN_RESULTS]: this.results,
+      [GetCustomerAccountDataFields.COLUMN_SEVERITIES_AGG]: this.severitiesAgg,
+      [GetCustomerAccountDataFields.COLUMN_STANDARDS_AGG]: this.standardsAgg,
+      [GetCustomerAccountDataFields.COLUMN_STANDARDS]: this.standards,
     };
   }
 }
--- src/securityScore/entities/getCustomerData.ts
+++ src/securityScore/entities/getCustomerData.ts
@@ -4,22 +4,26 @@ import {
   FilterType,
   ScoreResultType,
   MonthlyTrendAggType,
-  IssuesAggType,
   SeveritiesAggType,
+  PaginationsType,
   PeriodsType,
   StandardType,
+  ChecksAggType,
+  ScoresAggType,
 } from '../types/securityScoreGraphQLTypes';
 import { SecurityScoreQueries } from '../types/queryArguments';
 
 export enum GetCustomerDataFields {
   COLUMN_ACCOUNTS_AGG = 'accountsAgg',
   COLUMN_AVG_CURRENT_SCORE = 'avgCurrentScore',
+  COLUMN_CHECKS_AGG = 'checksAgg',
   COLUMN_FILTERS = 'filters',
   COLUMN_MONTHLY_TREND_AGG = 'monthlyTrendAgg',
+  COLUMN_PAGINATION = 'pagination',
   COLUMN_PERIOD = 'period',
   COLUMN_RESULTS = 'results',
-  COLUMN_ISSUE_AGG = 'issueAgg',
-  COLUMN_SEVERITY_AGG = 'severityAgg',
+  COLUMN_SCORES_AGG = 'scoresAgg',
+  COLUMN_SEVERITIES_AGG = 'severitiesAgg',
   COLUMN_STANDARDS = 'standards',
   COLUMN_SUBSCRIPTION_REFERENCES = 'subscriptionReferences',
 }
@@ -27,12 +31,14 @@ export enum GetCustomerDataFields {
 export type GetCustomerDataType = {
   [GetCustomerDataFields.COLUMN_ACCOUNTS_AGG]?: AccountsAggType;
   [GetCustomerDataFields.COLUMN_AVG_CURRENT_SCORE]?: number;
+  [GetCustomerDataFields.COLUMN_CHECKS_AGG]?: ChecksAggType;
   [GetCustomerDataFields.COLUMN_FILTERS]?: [FilterType];
   [GetCustomerDataFields.COLUMN_MONTHLY_TREND_AGG]?: MonthlyTrendAggType;
+  [GetCustomerDataFields.COLUMN_PAGINATION]?: PaginationsType;
   [GetCustomerDataFields.COLUMN_PERIOD]?: PeriodsType;
   [GetCustomerDataFields.COLUMN_RESULTS]?: [ScoreResultType];
-  [GetCustomerDataFields.COLUMN_ISSUE_AGG]?: IssuesAggType;
-  [GetCustomerDataFields.COLUMN_SEVERITY_AGG]?: SeveritiesAggType;
+  [GetCustomerDataFields.COLUMN_SCORES_AGG]?: ScoresAggType;
+  [GetCustomerDataFields.COLUMN_SEVERITIES_AGG]?: SeveritiesAggType;
   [GetCustomerDataFields.COLUMN_STANDARDS]?: [StandardType];
   [GetCustomerDataFields.COLUMN_SUBSCRIPTION_REFERENCES]?: number;
 };
@@ -44,12 +50,14 @@ export type GetCustomerDataGraphQLResultType = {
 export class GetCustomerData extends AbstractEntity<GetCustomerDataType> {
   readonly #accountsAgg?: AccountsAggType;
   readonly #avgCurrentScore?: number;
+  readonly #checksAgg?: ChecksAggType;
   readonly #filters?: [FilterType];
   readonly #monthlyTrendAgg?: MonthlyTrendAggType;
+  readonly #pagination?: PaginationsType;
   readonly #period?: PeriodsType;
   readonly #results?: [ScoreResultType];
-  readonly #issueAgg?: IssuesAggType;
-  readonly #severityAgg?: SeveritiesAggType;
+  readonly #scoresAgg?: ScoresAggType;
+  readonly #severitiesAgg?: SeveritiesAggType;
   readonly #standards?: [StandardType];
   readonly #subscriptionReferences?: number;
 
@@ -60,15 +68,19 @@ export class GetCustomerData extends AbstractEntity<GetCustomerDataType> {
       getCustomerDataInput[GetCustomerDataFields.COLUMN_ACCOUNTS_AGG];
     this.#avgCurrentScore =
       getCustomerDataInput[GetCustomerDataFields.COLUMN_AVG_CURRENT_SCORE];
+    this.#checksAgg =
+      getCustomerDataInput[GetCustomerDataFields.COLUMN_CHECKS_AGG];
     this.#filters = getCustomerDataInput[GetCustomerDataFields.COLUMN_FILTERS];
     this.#monthlyTrendAgg =
       getCustomerDataInput[GetCustomerDataFields.COLUMN_MONTHLY_TREND_AGG];
+    this.#pagination =
+      getCustomerDataInput[GetCustomerDataFields.COLUMN_PAGINATION];
     this.#period = getCustomerDataInput[GetCustomerDataFields.COLUMN_PERIOD];
     this.#results = getCustomerDataInput[GetCustomerDataFields.COLUMN_RESULTS];
-    this.#issueAgg =
-      getCustomerDataInput[GetCustomerDataFields.COLUMN_ISSUE_AGG];
-    this.#severityAgg =
-      getCustomerDataInput[GetCustomerDataFields.COLUMN_SEVERITY_AGG];
+    this.#scoresAgg =
+      getCustomerDataInput[GetCustomerDataFields.COLUMN_SCORES_AGG];
+    this.#severitiesAgg =
+      getCustomerDataInput[GetCustomerDataFields.COLUMN_SEVERITIES_AGG];
     this.#standards =
       getCustomerDataInput[GetCustomerDataFields.COLUMN_STANDARDS];
     this.#subscriptionReferences =
@@ -85,12 +97,20 @@ export class GetCustomerData extends AbstractEntity<GetCustomerDataType> {
     return this.#avgCurrentScore;
   }
 
+  get checksAgg(): ChecksAggType | undefined {
+    return this.#checksAgg;
+  }
+
   get filters(): [FilterType] | undefined {
     return this.#filters;
   }
 
   get monthlyTrendAgg(): MonthlyTrendAggType | undefined {
     return this.#monthlyTrendAgg;
+  }
+
+  get pagination(): PaginationsType | undefined {
+    return this.#pagination;
   }
 
   get period(): PeriodsType | undefined {
@@ -101,8 +121,12 @@ export class GetCustomerData extends AbstractEntity<GetCustomerDataType> {
     return this.#results;
   }
 
-  get issueAgg(): IssuesAggType | undefined {
-    return this.#issueAgg;
+  get scoresAgg(): ScoresAggType | undefined {
+    return this.#scoresAgg;
+  }
+
+  get severitiesAgg(): SeveritiesAggType | undefined {
+    return this.#severitiesAgg;
   }
 
   get standards(): [StandardType] | undefined {
@@ -115,17 +139,19 @@ export class GetCustomerData extends AbstractEntity<GetCustomerDataType> {
 
   public toJSON(): GetCustomerDataType {
     return {
-      [GetCustomerDataFields.COLUMN_ACCOUNTS_AGG]: this.#accountsAgg,
-      [GetCustomerDataFields.COLUMN_AVG_CURRENT_SCORE]: this.#avgCurrentScore,
-      [GetCustomerDataFields.COLUMN_FILTERS]: this.#filters,
-      [GetCustomerDataFields.COLUMN_MONTHLY_TREND_AGG]: this.#monthlyTrendAgg,
-      [GetCustomerDataFields.COLUMN_PERIOD]: this.#period,
-      [GetCustomerDataFields.COLUMN_RESULTS]: this.#results,
-      [GetCustomerDataFields.COLUMN_ISSUE_AGG]: this.#issueAgg,
-      [GetCustomerDataFields.COLUMN_SEVERITY_AGG]: this.#severityAgg,
-      [GetCustomerDataFields.COLUMN_STANDARDS]: this.#standards,
+      [GetCustomerDataFields.COLUMN_ACCOUNTS_AGG]: this.accountsAgg,
+      [GetCustomerDataFields.COLUMN_AVG_CURRENT_SCORE]: this.avgCurrentScore,
+      [GetCustomerDataFields.COLUMN_CHECKS_AGG]: this.checksAgg,
+      [GetCustomerDataFields.COLUMN_FILTERS]: this.filters,
+      [GetCustomerDataFields.COLUMN_MONTHLY_TREND_AGG]: this.monthlyTrendAgg,
+      [GetCustomerDataFields.COLUMN_PAGINATION]: this.pagination,
+      [GetCustomerDataFields.COLUMN_PERIOD]: this.period,
+      [GetCustomerDataFields.COLUMN_RESULTS]: this.results,
+      [GetCustomerDataFields.COLUMN_SCORES_AGG]: this.scoresAgg,
+      [GetCustomerDataFields.COLUMN_SEVERITIES_AGG]: this.severitiesAgg,
+      [GetCustomerDataFields.COLUMN_STANDARDS]: this.standards,
       [GetCustomerDataFields.COLUMN_SUBSCRIPTION_REFERENCES]: this
-        .#subscriptionReferences,
+        .subscriptionReferences,
     };
   }
 }
