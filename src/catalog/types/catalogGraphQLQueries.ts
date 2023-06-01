f23646b09495ce2bc0afc853383ad48fdf19f3a2
JPB <jpb.blond@arrow.com>
2023-06-01 16:48:25 +0200

    refactor(catalog-graphql): find products by query feat(catalog-graphql): find one priceBand by query feat(catalog-graphql): find one product by query feat(catalog-graphql): fetch promotions refactor(catalog-graphql): rename SearchFilterArgument to SearchProductFilterArgument to avoid name collision

--- src/catalog/types/catalogGraphQLQueries.ts
+++ src/catalog/types/catalogGraphQLQueries.ts
@@ -1,18 +1,46 @@
-import { QueryArguments } from './queryArguments';
+import {
+  QueryPriceBandArguments,
+  QueryProductArguments,
+} from './queryProductArguments';
 import {
   FiltersSchema,
   PaginationSchema,
+  PriceBandSchema,
   ProductSchema,
 } from './catalogGraphQLSchemas';
+import { Merge } from 'type-fest';
 
+/**
+ * @deprecated
+ * Prefer using CatalogQueries instead.
+ */
 export type CatalogQuery = {
   getProducts: GetPaginatedProductsQuery;
 };
 
-export declare type GetPaginatedProductsQuery = {
-  __args: QueryArguments;
+/**
+ * Represent the Catalog Schema of Public API
+ */
+export type CatalogQueries = {
+  getProducts?: GetPaginatedProductsQuery;
+  priceBand?: GetPriceBandQuery;
+  product?: GetProductQuery;
+};
+
+export type GetPaginatedProductsQuery = {
+  __args: QueryProductArguments;
   filters?: FiltersSchema;
   pagination?: PaginationSchema;
   products?: ProductSchema;
   topOffers?: ProductSchema;
 };
+
+export type GetProductQuery = Merge<
+  { __args: QueryProductArguments },
+  ProductSchema
+>;
+
+export type GetPriceBandQuery = Merge<
+  { __args: QueryPriceBandArguments },
+  PriceBandSchema
+>;
