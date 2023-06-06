1e87e5ca4c34ea95ebcef24a27e183dd4089382e
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2023-06-06 16:57:29 +0200

    security score update type  Check

--- src/securityScore/types/securityScoreGraphQLTypes.ts
+++ src/securityScore/types/securityScoreGraphQLTypes.ts
@@ -43,6 +43,7 @@ export type RegistrationType = {
 export type CheckType = {
   description?: string;
   flagged?: number;
+  group?: string;
   isFailed?: boolean;
   name?: string;
   processed?: number;
