fb8440f2918bf55f343c86e9cbe57e853af495b0
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2023-07-21 11:17:00 +0200

    rebase master

--- src/publicGraphQLClient.ts
+++ src/publicGraphQLClient.ts
@@ -5,29 +5,31 @@ import { GraphqlApiClient } from './graphqlApi';
 
 export class PublicGraphQLClient extends AbstractGraphQLClient {
   public getCatalogGraphQLClient(): CatalogGraphQLClient {
-    return new CatalogGraphQLClient()
-      .setUrl(this.url)
-      .setToken(this.token)
-      .setHeaders(this.headers)
-      .setToken(this.token)
-      .setHttpExceptionHandlers(this.httpExceptionHandlers);
+    const client = new CatalogGraphQLClient();
+    this.applyConfig(client);
+
+    return client;
   }
 
   public getSecurityScoreGraphQLClient(): SecurityScoreGraphQLClient {
-    return new SecurityScoreGraphQLClient()
-      .setUrl(this.url)
-      .setToken(this.token)
-      .setHeaders(this.headers)
-      .setToken(this.token)
-      .setHttpExceptionHandlers(this.httpExceptionHandlers);
+    const client = new SecurityScoreGraphQLClient();
+    this.applyConfig(client);
+
+    return client;
   }
 
   public getGraphqlApiClient(): GraphqlApiClient {
-    return new GraphqlApiClient()
+    const client = new GraphqlApiClient();
+    this.applyConfig(client);
+
+    return client;
+  }
+
+  private applyConfig(client: AbstractGraphQLClient): AbstractGraphQLClient {
+    return client
       .setUrl(this.url)
-      .setToken(this.token)
+      .setSecurity(this.security)
       .setHeaders(this.headers)
-      .setToken(this.token)
       .setHttpExceptionHandlers(this.httpExceptionHandlers);
   }
 }
