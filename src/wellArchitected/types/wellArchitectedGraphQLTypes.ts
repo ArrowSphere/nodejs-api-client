1b566ff709fb5c2eba4a9335a6d4f9172882d727
Nicolas Foiny <nicolas.foiny@arrow.com>
2023-09-06 10:50:25 +0200

    Well Architected support

---
+++ src/wellArchitected/types/wellArchitectedGraphQLTypes.ts
@@ -0,0 +1,388 @@
+/**
+ * Filter Types
+ */
+export type FilterType = {
+  name?: string;
+  values?: FilterValuesType[];
+};
+
+export type FilterValuesType = {
+  count?: number;
+  value?: string;
+};
+
+export type WellArchitectedPaginationType = {
+  currentPage?: number;
+  next?: string;
+  perPage?: number;
+  previous?: string;
+  total?: number;
+  totalPage?: number;
+};
+
+/**
+ * ElasticSearch Types
+ */
+export type SubscriptionRegistrationType = {
+  reference: string;
+};
+
+export type CustomerRegistrationType = {
+  name?: string;
+  reference?: string;
+};
+
+export type ResellerRegistrationType = {
+  name?: string;
+  reference?: string;
+};
+
+export type RegistrationType = {
+  accountReference?: string;
+  customer?: CustomerRegistrationType;
+  marketplace?: string;
+  reseller?: ResellerRegistrationType;
+  subscription?: SubscriptionRegistrationType;
+  vendorCode?: string;
+};
+
+export type CheckType = {
+  description?: string;
+  flagged?: number;
+  group?: string;
+  isFailed?: boolean;
+  name?: string;
+  processed?: number;
+  reference?: string;
+  score?: number;
+  severity?: string;
+};
+
+export type StandardType = {
+  currentScore?: number;
+  checks?: CheckType[];
+  failed?: number;
+  maxScore?: number;
+  name?: string;
+  passed?: number;
+  reference?: string;
+  score?: number;
+  total?: number;
+};
+
+export type AccountType = {
+  failed?: number;
+  name?: string;
+  passed?: number;
+  reference?: string;
+  score?: number;
+  standards?: StandardType[];
+  total?: number;
+};
+
+export type ScoreResultType = {
+  account?: AccountType;
+  registration?: RegistrationType;
+};
+
+/**
+ * Generic Types
+ */
+export type WellArchitectedPeriodType = {
+  from?: string;
+  to?: string;
+};
+
+/**
+ * Severity Aggregation Types
+ */
+export type SeverityByDateAggType = {
+  date: string;
+  value: number;
+};
+
+export type SeverityAggType = {
+  data: SeverityByDateAggType[];
+  last: SeverityByDateAggType;
+  name: string;
+  progression: number;
+};
+
+export type SeveritiesAggType = {
+  severities: SeverityAggType[];
+};
+
+/**
+ * Check Aggregation Types
+ */
+export type CheckCountByDateAggType = {
+  accounts?: number;
+  count?: number;
+  customers?: number;
+  date?: string;
+  partners?: number;
+};
+
+export type CheckAggType = {
+  data?: CheckCountByDateAggType[];
+  last?: CheckCountByDateAggType;
+  name?: string;
+  progression?: number;
+  reference?: string;
+  vendorCode?: string;
+};
+
+export type ChecksAggType = {
+  checks: CheckAggType[];
+};
+
+/**
+ * Subscription Aggregation Types
+ */
+export type SubscriptionAggType = {
+  reference: string;
+  updatedAt: string;
+};
+
+export type SubscriptionsAggType = {
+  count: number;
+  subscriptions: SubscriptionAggType[];
+};
+
+/**
+ * EndCustomer Aggregation Types
+ */
+export type EndCustomerByDateAggType = {
+  accounts?: number;
+  date?: string;
+  failed?: number;
+  name?: string;
+  passed?: number;
+  score?: number;
+  scoreUnit?: string;
+  subscriptionReferences?: number;
+  total?: number;
+};
+
+export type EndCustomerAggType = {
+  data?: EndCustomerByDateAggType[];
+  last?: EndCustomerByDateAggType;
+  name?: string;
+  progression?: number;
+  reference?: string;
+};
+
+export type UnregisteredOfferIaasSubscriptionType = {
+  partnerRef?: string;
+  friendlyName?: string;
+  vendorSubscriptionId?: string;
+};
+
+export type UnregisteredOfferIaasType = {
+  sku?: string;
+  name?: string;
+  subscriptions?: UnregisteredOfferIaasSubscriptionType[];
+};
+
+export type UnregisteredOfferSaasType = {
+  name?: string;
+  subscription?: string;
+};
+
+export type UnregisteredOffersType = {
+  iaas?: UnregisteredOfferIaasType[];
+  saas?: UnregisteredOfferSaasType[];
+};
+
+export type UnregisteredEndCustomerAggType = {
+  name?: string;
+  reference?: string;
+  offers?: UnregisteredOffersType;
+};
+
+export type EndCustomersAggType = {
+  customers?: EndCustomerAggType[];
+  unregisteredCustomers?: UnregisteredEndCustomerAggType[];
+};
+
+/**
+ * Partners Aggregation Types
+ */
+export type PartnerByDateAggType = {
+  date?: string;
+  failed?: number;
+  name?: string;
+  passed?: number;
+  score?: number;
+  scoreUnit?: string;
+  total?: number;
+};
+
+export type PartnerAggType = {
+  data?: PartnerByDateAggType[];
+  last?: PartnerByDateAggType;
+  name?: string;
+  progression?: number;
+  reference?: string;
+};
+
+export type PartnersAggType = {
+  partners?: PartnerAggType[];
+};
+
+/**
+ * Monthly Aggregation Types
+ */
+export type ScoreByMonthSeverityType = {
+  name: string;
+  value: number;
+};
+
+export type ScoreByMonthAggType = {
+  date: string;
+  score?: number;
+  scoreUnit?: string;
+  severities?: ScoreByMonthSeverityType[];
+};
+
+export type MonthlyTrendAggType = {
+  period?: WellArchitectedPeriodType;
+  score?: number;
+  scores?: ScoreByMonthAggType[];
+  scoreUnit?: string;
+};
+
+/**
+ * Score Aggregation Types
+ */
+export type ScoreByDateAggType = {
+  date?: string;
+  failed?: number;
+  passed?: number;
+  score?: number;
+  scoreUnit?: string;
+  total?: number;
+};
+
+export type ScoresAggType = {
+  last?: ScoreByDateAggType;
+  progression?: number;
+  scores?: ScoreByDateAggType[];
+};
+
+/**
+ * Account Aggregation Types
+ */
+export type AccountByDateAggType = {
+  date?: string;
+  failed?: number;
+  name?: string;
+  passed?: number;
+  score?: number;
+  scoreUnit?: string;
+  total?: number;
+};
+
+export type AccountAggType = {
+  data?: AccountByDateAggType[];
+  last?: AccountByDateAggType;
+  name?: string;
+  progression?: number;
+  reference?: string;
+};
+
+export type AccountsAggType = {
+  accounts?: AccountAggType[];
+  unregisteredAccounts?: UnregisteredOffersType;
+};
+
+/**
+ * Standard Aggregation Types
+ */
+export type StandardByDateAggType = {
+  date?: string;
+  failed?: number;
+  passed?: number;
+  score?: number;
+  scoreUnit?: string;
+  total?: number;
+};
+
+export type StandardAggType = {
+  data?: StandardByDateAggType[];
+  last?: StandardByDateAggType;
+  name?: string;
+  progression?: number;
+  reference?: string;
+};
+
+export type StandardsAggType = {
+  standards: StandardAggType[];
+};
+
+/**
+ * Marketplace Aggregation Types
+ */
+export type MarketplacePartnerAggByDateAggType = {
+  date?: string;
+  failed?: number;
+  passed?: number;
+  score?: number;
+  scoreUnit?: string;
+  total?: number;
+};
+
+export type MarketplaceByDateAggType = {
+  date?: string;
+  score?: number;
+  scoreUnit?: string;
+};
+
+export type MarketplacePartnerAggType = {
+  data?: MarketplacePartnerAggByDateAggType[];
+  last?: MarketplacePartnerAggByDateAggType;
+  name?: string;
+  reference?: string;
+  progression?: number;
+};
+
+export type MarketplaceAggType = {
+  data?: MarketplaceByDateAggType[];
+  last?: MarketplaceByDateAggType;
+  name?: string;
+  partners?: MarketplacePartnerAggType[];
+  progression?: number;
+};
+
+export type MarketplacesAggType = {
+  marketplaces: MarketplaceAggType[];
+};
+
+/**
+ * Standard with Check Types
+ */
+export type CheckByDateType = {
+  date?: string;
+  flagged?: number;
+  isFailed?: boolean;
+  name?: string;
+  processed?: number;
+  score?: number;
+  scoreUnit?: string;
+};
+
+export type ChecksByStandardType = {
+  data?: CheckByDateType[];
+  description?: string;
+  last?: CheckByDateType;
+  name?: string;
+  progression?: number;
+  reference?: string;
+  severity?: string;
+};
+
+export type StandardWithCheckType = {
+  checks: ChecksByStandardType[];
+  name?: string;
+  reference?: string;
+};
---
+++ src/wellArchitected/wellArchitectedGraphQLClient.ts
@@ -0,0 +1,126 @@
+import { AbstractGraphQLClient } from '../abstractGraphQLClient';
+import {
+  GetPartnerData,
+  GetPartnerDataType,
+  GetPartnerDataGraphQLResultType,
+} from './entities/getPartnerData';
+import {
+  GetCustomerData,
+  GetCustomerDataType,
+  GetCustomerDataGraphQLResultType,
+} from './entities/getCustomerData';
+import {
+  GetCustomerAccountData,
+  GetCustomerAccountDataType,
+  GetCustomerAccountDataGraphQLResultType,
+} from './entities/getCustomerAccountData';
+import { WellArchitectedQueries } from './types/queryArguments';
+import {
+  GetPartnerDataQuery,
+  GetCustomerDataQuery,
+  GetCustomerAccountDataQuery,
+  GetAdminDataQuery,
+} from './types/wellArchitectedGraphQLQueries';
+import {
+  GetAdminData,
+  GetAdminDataType,
+  GetAdminDataGraphQLResultType,
+} from './entities/getAdminData';
+
+export class WellArchitectedGraphQLClient extends AbstractGraphQLClient {
+  /**
+   * The base path of the API
+   */
+  protected basePath = '/wellArchitected';
+
+  /**
+   * The Path of graphql catalog API
+   */
+  private GRAPHQL = '/graphql/score';
+
+  public async find<GraphQLResponseTypes>(
+    request: string,
+  ): Promise<GraphQLResponseTypes | null> {
+    this.path = this.GRAPHQL;
+
+    try {
+      return await this.post<GraphQLResponseTypes>(request);
+    } catch (error: unknown) {
+      return null;
+    }
+  }
+
+  public async getPartnerData(
+    getPartnerDataQuery: GetPartnerDataQuery,
+  ): Promise<GetPartnerDataType | null> {
+    const queryStr: string = this.stringifyQuery(getPartnerDataQuery);
+
+    const getPartnerDataResult: GetPartnerDataGraphQLResultType | null = await this.find<GetPartnerDataGraphQLResultType>(
+      queryStr,
+    );
+
+    if (getPartnerDataResult) {
+      return new GetPartnerData(
+        getPartnerDataResult[WellArchitectedQueries.GET_PARTNER_DATA],
+      ).toJSON();
+    }
+
+    return null;
+  }
+
+  public async getAdminData(
+    getAdminDataQuery: GetAdminDataQuery,
+  ): Promise<GetAdminDataType | null> {
+    const queryStr: string = this.stringifyQuery(getAdminDataQuery);
+
+    const getAdminDataResult: GetAdminDataGraphQLResultType | null = await this.find<GetAdminDataGraphQLResultType>(
+      queryStr,
+    );
+
+    if (getAdminDataResult) {
+      return new GetAdminData(
+        getAdminDataResult[WellArchitectedQueries.GET_ADMIN_DATA],
+      ).toJSON();
+    }
+
+    return null;
+  }
+
+  public async getCustomerData(
+    getCustomerDataQuery: GetCustomerDataQuery,
+  ): Promise<GetCustomerDataType | null> {
+    const queryStr: string = this.stringifyQuery(getCustomerDataQuery);
+
+    const getCustomerDataResult: GetCustomerDataGraphQLResultType | null = await this.find<GetCustomerDataGraphQLResultType>(
+      queryStr,
+    );
+
+    if (getCustomerDataResult) {
+      return new GetCustomerData(
+        getCustomerDataResult[WellArchitectedQueries.GET_CUSTOMER_DATA],
+      ).toJSON();
+    }
+
+    return null;
+  }
+
+  public async getCustomerAccountData(
+    getCustomerAccountDataQuery: GetCustomerAccountDataQuery,
+  ): Promise<GetCustomerAccountDataType | null> {
+    const queryStr: string = this.stringifyQuery(getCustomerAccountDataQuery);
+
+    const getCustomerAccountDataResult: GetCustomerAccountDataGraphQLResultType | null = await this.find<GetCustomerAccountDataGraphQLResultType>(
+      queryStr,
+    );
+
+    if (getCustomerAccountDataResult) {
+      return new GetCustomerAccountData(
+        getCustomerAccountDataResult[
+          WellArchitectedQueries.GET_CUSTOMER_ACCOUNT_DATA
+        ],
+      ).toJSON();
+    }
+
+    return null;
+  }
+}
