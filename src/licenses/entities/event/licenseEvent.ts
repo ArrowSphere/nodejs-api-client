13c5156c7d32738e396e128a5f4dcb77075c1b10
Wilfried EVIEUX <wilfried.evieux@arrow.com>
2023-11-08 11:40:10 +0100

    ARE-32374 - feat(license-events) add event type / action enums

--- src/licenses/entities/event/licenseEvent.ts
+++ src/licenses/entities/event/licenseEvent.ts
@@ -11,12 +11,52 @@ export enum LicenseEventActionType {
   SUSPENDED = 'suspended',
 }
 
+export enum LicenseEventType {
+  ACTIVATION_FAILURE = 'activation_failure',
+  ACTIVATION_SUCCESS = 'activation_success',
+  CANCELLED = 'cancelled',
+  CREATED = 'created',
+  PREPARATION = 'preparation',
+  SUSPENDED = 'suspended',
+  TRANSFERRED = 'transferred',
+  TRANSMISSION_FAILURE = 'transmission_failure',
+  TRANSMISSION_IN_PROGRESS = 'transmission_in_progress',
+  TRANSMISSION_SUCCESS = 'transmission_success',
+  UPDATED = 'updated',
+  PENDING = 'pending',
+}
+
 export enum LicenseEventStatusCode {
+  PREPARATION = '40',
+  TRANSMISSION_IN_PROGRESS = '42',
+  TRANSMISSION_FAILURE = '43',
   PENDING = '62',
+  TRANSMISSION_SUCCESS = '44',
   IN_PROGRESS = '72',
   REJECTED = '85',
   COMPLETED = '86',
+  SUSPENDED = '87',
+  CANCELLED = '89',
+  TRANSFERRED = '126',
 }
+
+export const LicenseEventStatusActionCodes: Partial<
+  Record<LicenseEventStatusCode, LicenseEventType>
+> = {
+  [LicenseEventStatusCode.PREPARATION]: LicenseEventType.PREPARATION,
+  [LicenseEventStatusCode.TRANSMISSION_IN_PROGRESS]:
+    LicenseEventType.TRANSMISSION_IN_PROGRESS,
+  [LicenseEventStatusCode.TRANSMISSION_FAILURE]:
+    LicenseEventType.TRANSMISSION_FAILURE,
+  [LicenseEventStatusCode.TRANSMISSION_SUCCESS]:
+    LicenseEventType.TRANSMISSION_SUCCESS,
+  [LicenseEventStatusCode.REJECTED]: LicenseEventType.ACTIVATION_FAILURE,
+  [LicenseEventStatusCode.COMPLETED]: LicenseEventType.ACTIVATION_SUCCESS,
+  [LicenseEventStatusCode.SUSPENDED]: LicenseEventType.SUSPENDED,
+  [LicenseEventStatusCode.CANCELLED]: LicenseEventType.CANCELLED,
+  [LicenseEventStatusCode.TRANSFERRED]: LicenseEventType.TRANSFERRED,
+  [LicenseEventStatusCode.PENDING]: LicenseEventType.PENDING,
+};
 
 export interface LicenseEventVendor {
   name: string;
