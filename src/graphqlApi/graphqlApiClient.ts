8aecfee067ab1e200ee0fdb83c1b4c71df224001
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2023-11-13 09:42:46 +0100

    graphql-api adds functions to select companies

--- src/graphqlApi/graphqlApiClient.ts
+++ src/graphqlApi/graphqlApiClient.ts
@@ -1,11 +1,32 @@
 import { AbstractGraphQLClient } from '../abstractGraphQLClient';
+import {
+  ArrowCompanyType,
+  EndCustomerType,
+  PartnerType,
+} from './types/entities/company';
 
 import {
+  ComparisonOperator,
+  ErrorsField,
+  InputFilterValueField,
+  InputFiltersField,
+  InputSearchFilterField,
+  Queries,
+  QueryVariablesField,
   SelectAllQueryType,
   SelectAllResultType,
+  SelectDataField,
   SelectOneQueryType,
+  SelectOneResponseDataType,
   SelectOneResultType,
+  SelectableField,
 } from './types/graphqlApiQueries';
+import {
+  ArrowCompanySchema,
+  EndCustomerSchema,
+  PartnerSchema,
+  SelectOneResponseDataSchema,
+} from './types/graphqlApiSchemas';
 
 export class GraphqlApiClient extends AbstractGraphQLClient {
   /**
@@ -47,5 +68,76 @@ export class GraphqlApiClient extends AbstractGraphQLClient {
     );
 
     return result;
+  }
+
+  public async findOneById<T>(
+    id: number,
+    fieldSchema: SelectOneResponseDataSchema,
+  ): Promise<T | null> {
+    const keys: string[] = Object.keys(fieldSchema);
+    if (keys.length === 0) {
+      return null;
+    }
+
+    const type: keyof SelectOneResponseDataType = keys[0] as keyof SelectOneResponseDataType;
+
+    const query: SelectOneQueryType = {
+      [Queries.SELECT_ONE]: {
+        __args: {
+          [QueryVariablesField.FILTERS]: {
+            [InputSearchFilterField.GROUPS]: [
+              {
+                [InputFiltersField.ITEMS]: [
+                  {
+                    [InputFilterValueField.NAME]: 'id',
+                    [InputFilterValueField.OPERATOR]: ComparisonOperator.EQUALS,
+                    [InputFilterValueField.VALUE]: [`${id}`],
+                  },
+                ],
+              },
+            ],
+          },
+        },
+        [SelectableField.DATA]: fieldSchema,
+        [SelectableField.ERRORS]: {
+          [ErrorsField.MESSAGE]: true,
+        },
+      },
+    };
+
+    const queryStr: string = this.stringifyQuery(query);
+
+    const result: SelectOneResultType | null = await this.find<SelectOneResultType>(
+      queryStr,
+    );
+
+    return result?.[Queries.SELECT_ONE]?.[SelectableField.DATA]?.[type] as T;
+  }
+
+  public async findEndCustomerById(
+    id: number,
+    fields: EndCustomerSchema,
+  ): Promise<EndCustomerType | null> {
+    return await this.findOneById<EndCustomerType>(id, {
+      [SelectDataField.END_CUSTOMER]: fields,
+    });
+  }
+
+  public async findPartnerById(
+    id: number,
+    fields: PartnerSchema,
+  ): Promise<PartnerType | null> {
+    return await this.findOneById<PartnerType>(id, {
+      [SelectDataField.PARTNER]: fields,
+    });
+  }
+
+  public async findArrowCompanyById(
+    id: number,
+    fields: ArrowCompanySchema,
+  ): Promise<ArrowCompanyType | null> {
+    return await this.findOneById<ArrowCompanyType>(id, {
+      [SelectDataField.ARROW_COMPANY]: fields,
+    });
   }
 }
