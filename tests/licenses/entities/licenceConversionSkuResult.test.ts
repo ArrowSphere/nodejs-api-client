105ac04a21930b9336ffc7fe2eb756c74da2cb41
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2023-12-06 18:52:25 +0100

    license conversion sku

---
+++ tests/licenses/entities/licenceConversionSkuResult.test.ts
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import {
+  LicenseConversionSkuResult,
+  LicenseConversionSkuResultData,
+} from '../../../src';
+
+describe('License Conversion Sku', () => {
+  it('License Conversion Sku for existing', () => {
+    const input: LicenseConversionSkuResultData = {
+      offers: [
+        {
+          billingCycle: 720,
+          licenseReference: 'XSP5046568',
+          name: 'Office 365 E3',
+          seats: 1,
+          sku: 'CFQ7TTC0LF8R:0001',
+          term: 8640,
+        },
+      ],
+    };
+
+    const result = new LicenseConversionSkuResult(input);
+    expect(result.toJSON()).to.deep.equal(input);
+  });
+
+  it('License Conversion Sku', () => {
+    const input: LicenseConversionSkuResultData = {
+      offers: [
+        {
+          billingCycle: 720,
+          name: 'Office 365 E3',
+          sku: 'CFQ7TTC0LF8R:0001',
+          term: 8640,
+        },
+      ],
+    };
+
+    const result = new LicenseConversionSkuResult(input);
+    expect(result.toJSON()).to.deep.equal(input);
+  });
+});
