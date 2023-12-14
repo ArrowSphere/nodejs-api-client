6a474e2f7c41ed27105e18da5e3ee3ab57377140
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2023-12-14 15:46:44 +0100

    Fixes get LicenseRequest data structure

--- src/licenses/licenseRequestClient.ts
+++ src/licenses/licenseRequestClient.ts
@@ -2,17 +2,37 @@
  * Class LicenseRequestClient
  */
 import { AbstractRestfulClient } from '../abstractRestfulClient';
-import { GetResult } from '../getResult';
-import { LicenseRequest } from './entities/request/licenseRequest';
+import { GetResult, GetResultFields } from '../getResult';
+import {
+  LicenseRequestResult,
+  LicenseRequestResultFields,
+  LicenseRequestType,
+} from './entities/request/licenseRequest';
 
 export class LicenseRequestClient extends AbstractRestfulClient {
   protected basePath = '/licenses';
 
   public async getLastRequests(
     licenseReference: string,
-  ): Promise<GetResult<LicenseRequest>> {
+  ): Promise<LicenseRequestType[]> {
     this.path = `/${licenseReference}/request`;
 
-    return new GetResult(LicenseRequest, await this.get());
+    const response = await this.get();
+    //A workaround, the public api endpoint is not returning "data" in the payload
+    response[GetResultFields.COLUMN_DATA] = {
+      [LicenseRequestResultFields.COLUMN_LICENSE_REQUEST]:
+        GetResultFields.COLUMN_DATA in response
+          ? response[GetResultFields.COLUMN_DATA]
+          : [],
+    };
+
+    const result: GetResult<LicenseRequestResult> = new GetResult(
+      LicenseRequestResult,
+      response,
+    );
+
+    return result.toJSON()[GetResultFields.COLUMN_DATA][
+      LicenseRequestResultFields.COLUMN_LICENSE_REQUEST
+    ] as LicenseRequestType[];
   }
 }
