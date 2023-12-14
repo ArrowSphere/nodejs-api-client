3879db329f8e1d767f0206a97b5f14883bcfc6e2
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2023-12-14 15:46:44 +0100

    Fixes get LicenseRequest data structure

--- tests/licenses/entities/licenceRequest.test.ts
+++ tests/licenses/entities/licenceRequest.test.ts
@@ -1,20 +1,40 @@
 import { expect } from 'chai';
-import { LicenseRequest, LicenseRequestType } from '../../../src';
+import { LicenseRequestResult, LicenseRequestType } from '../../../src';
 
 describe('License Request', () => {
   it('GetLicenceRequest toJSON', () => {
-    const input: LicenseRequestType = {
-      licenseReference: 'XSP4289490',
-      action: 'updateFriendlyName',
-      status: 'In progress',
-      userName: 'API xCP',
-      createdAt: '2023-10-31 16:51:00',
-    };
-    const result = new LicenseRequest(input);
+    const input: LicenseRequestType[] = [
+      {
+        licenseReference: 'XSP123',
+        action: 'updateFriendlyName',
+        status: 'In progress',
+        userName: 'API xCP',
+        createdAt: '2023-10-31 16:51:00',
+      },
+      {
+        licenseReference: 'XSP456',
+        action: 'setAutoRenewOn',
+        status: 'Done',
+        userName: 'API xCP',
+        createdAt: '2023-10-31 12:45:00',
+        message: 'The license has been updated',
+        updatedAt: '2023-10-31 12:46:00',
+      },
+    ];
+
+    const result = new LicenseRequestResult(input);
+
     expect(result.toJSON()).to.deep.equal({
-      ...input,
-      message: undefined,
-      updatedAt: undefined,
+      licenseRequests: [
+        {
+          ...input[0],
+          message: undefined,
+          updatedAt: undefined,
+        },
+        {
+          ...input[1],
+        },
+      ],
     });
   });
 });
