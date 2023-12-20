c0d72a0e3771fd281b7e80d2878508d05201c99d
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2023-12-20 17:30:47 +0100

    add new column in get a license result

--- src/licenses/entities/getLicense/buySellFindResult.ts
+++ src/licenses/entities/getLicense/buySellFindResult.ts
@@ -3,22 +3,26 @@ import { AbstractEntity } from '../../../abstractEntity';
 export enum BuySellFields {
   COLUMN_BUY = 'buy',
   COLUMN_SELL = 'sell',
+  COLUMN_LIST = 'list',
 }
 
 export type BuySellData = {
   [BuySellFields.COLUMN_BUY]?: number;
   [BuySellFields.COLUMN_SELL]?: number;
+  [BuySellFields.COLUMN_LIST]?: number;
 };
 
 export class BuySellFindResult extends AbstractEntity<BuySellData> {
   readonly #buy?: number;
   readonly #sell?: number;
+  readonly #list?: number;
 
   public constructor(data: BuySellData) {
     super(data);
 
     this.#buy = data[BuySellFields.COLUMN_BUY];
     this.#sell = data[BuySellFields.COLUMN_SELL];
+    this.#list = data[BuySellFields.COLUMN_LIST];
   }
 
   public get buy(): number | undefined {
@@ -29,10 +33,15 @@ export class BuySellFindResult extends AbstractEntity<BuySellData> {
     return this.#sell;
   }
 
+  public get list(): number | undefined {
+    return this.#list;
+  }
+
   public toJSON(): BuySellData {
     return {
       [BuySellFields.COLUMN_BUY]: this.buy,
       [BuySellFields.COLUMN_SELL]: this.sell,
+      [BuySellFields.COLUMN_LIST]: this.list,
     };
   }
 }
