3a25ff7ed192e3444d4608613f4ef814d6c3b771
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2024-01-15 18:04:08 +0100

    graphql-api add entities vendor subscribedProgram

--- src/graphqlApi/types/entities/program.ts
+++ src/graphqlApi/types/entities/program.ts
@@ -1,5 +1,25 @@
+import { PartnerType } from './company';
+import { SubscriptionType } from './subscription';
+import { VendorsType } from './vendor';
+
 export type ProgramType = {
   id?: number;
   internalName?: string;
   name?: string;
+  vendor?: VendorsType;
+};
+
+export type SubscribedProgramType = {
+  id?: number;
+  availabilityEndedAt?: string;
+  availabilityStartedAt?: string;
+  companyId?: number;
+  internalName?: string;
+  subscriptionEndedAt?: string;
+  partner?: PartnerType;
+  program?: ProgramType;
+  subscription?: SubscriptionType;
+  vendor?: VendorsType;
+  vendorCode?: string;
+  vendorName?: string;
 };
