e22de29cbcc276700f01517cf9478b775e64a419
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2024-04-05 19:01:44 +0200

    list scheduled tasks

---
+++ src/licenses/entities/schedule/getSchedulesTasksResult.ts
@@ -0,0 +1,39 @@
+import { AbstractEntity } from '../../../abstractEntity';
+import {
+  GetScheduleTaskResult,
+  GetScheduleTaskResultData,
+} from './getScheduleTaskResult';
+
+export enum GetSchedulesTasksResultFields {
+  COLUMN_SCHEDULED_TASKS = 'scheduledTasks',
+}
+
+export type GetSchedulesTasksResultData = {
+  [GetSchedulesTasksResultFields.COLUMN_SCHEDULED_TASKS]: GetScheduleTaskResultData[];
+};
+
+export class GetSchedulesTasksResult extends AbstractEntity<GetSchedulesTasksResultData> {
+  readonly #scheduledTasks: GetScheduleTaskResult[];
+
+  public constructor(dataInput: GetSchedulesTasksResultData) {
+    super(dataInput);
+
+    this.#scheduledTasks = dataInput[
+      GetSchedulesTasksResultFields.COLUMN_SCHEDULED_TASKS
+    ].map(
+      (result: GetScheduleTaskResultData) => new GetScheduleTaskResult(result),
+    );
+  }
+
+  get scheduledTasks(): GetScheduleTaskResult[] {
+    return this.#scheduledTasks;
+  }
+
+  public toJSON(): GetSchedulesTasksResultData {
+    return {
+      [GetSchedulesTasksResultFields.COLUMN_SCHEDULED_TASKS]: this.scheduledTasks.map(
+        (result: GetScheduleTaskResult) => result.toJSON(),
+      ),
+    };
+  }
+}
