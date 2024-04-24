cb01f4259a4ef8d58d6e44f005e459ee1e2f2edd
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2024-04-24 07:36:01 +0200

    add field price.unit.init to get license endpoint

--- src/licenses/entities/getLicense/buySellFindResult.ts
+++ src/licenses/entities/getLicense/buySellFindResult.ts
@@ -4,18 +4,21 @@ export enum BuySellFields {
   COLUMN_BUY = 'buy',
   COLUMN_SELL = 'sell',
   COLUMN_LIST = 'list',
+  COLUMN_INIT = 'init',
 }
 
 export type BuySellData = {
   [BuySellFields.COLUMN_BUY]?: number;
   [BuySellFields.COLUMN_SELL]?: number;
   [BuySellFields.COLUMN_LIST]?: number;
+  [BuySellFields.COLUMN_INIT]?: number;
 };
 
 export class BuySellFindResult extends AbstractEntity<BuySellData> {
   readonly #buy?: number;
   readonly #sell?: number;
   readonly #list?: number;
+  readonly #init?: number;
 
   public constructor(data: BuySellData) {
     super(data);
@@ -23,6 +26,7 @@ export class BuySellFindResult extends AbstractEntity<BuySellData> {
     this.#buy = data[BuySellFields.COLUMN_BUY];
     this.#sell = data[BuySellFields.COLUMN_SELL];
     this.#list = data[BuySellFields.COLUMN_LIST];
+    this.#init = data[BuySellFields.COLUMN_INIT];
   }
 
   public get buy(): number | undefined {
@@ -37,11 +41,16 @@ export class BuySellFindResult extends AbstractEntity<BuySellData> {
     return this.#list;
   }
 
+  public get init(): number | undefined {
+    return this.#init;
+  }
+
   public toJSON(): BuySellData {
     return {
       [BuySellFields.COLUMN_BUY]: this.buy,
       [BuySellFields.COLUMN_SELL]: this.sell,
       [BuySellFields.COLUMN_LIST]: this.list,
+      [BuySellFields.COLUMN_INIT]: this.init,
     };
   }
 }
