1d366e97691ae38a31d2df494c9bc803bd275a0e
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2024-05-07 19:47:37 +0200

    [graphql-api] add entity userHistory

---
+++ src/graphqlApi/types/entities/user.ts
@@ -0,0 +1,18 @@
+import { ContactsType } from './contact';
+
+export type UserType = {
+  id?: number;
+  allowDirectLogin?: boolean;
+  canBeDeleted?: boolean;
+  contact?: ContactsType;
+  validatedAt?: string;
+};
+
+export type UserHistoryType = {
+  id?: number;
+  action?: string;
+  createdAt?: string;
+  description?: string;
+  impactedUser?: UserType;
+  originatorUser?: UserType;
+};
