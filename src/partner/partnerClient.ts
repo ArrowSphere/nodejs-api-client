c92ebe7826beb95a92a2c7cb4492cb08292e5670
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2024-05-15 20:14:01 +0200

    patners patch user

--- src/partner/partnerClient.ts
+++ src/partner/partnerClient.ts
@@ -1,5 +1,37 @@
 import { AbstractRestfulClient, Parameters } from '../abstractRestfulClient';
 
+export enum UpdateUserActionEnum {
+  ALLOW_DIRECT_LOGIN = 'allow-direct-login',
+  BLOCK_DIRECT_LOGIN = 'block-direct-login',
+  DISABLE_ALIAS = 'disable-alias',
+  LOCK = 'lock',
+  DISABLE_MFA = 'remove-mfa',
+  UNLOCK = 'unlock',
+  UNLOCK_INSECURE_LOGIN = 'unlock-insecure-login',
+  //VALIDATE = 'validate'
+}
+
+export enum PatchUserPayloadFields {
+  COLUMN_FIRSTNAME = 'firstname',
+  COLUMN_LASTNAME = 'lastname',
+  COLUMN_PHONE = 'phone',
+  COLUMN_TITLE = 'title',
+  COLUMN_IS_LOCKED = 'isLocked',
+  COLUMN_ORGANIZATION_UNIT_REF = 'organizationUnitRef',
+  COLUMN_ACTION = 'action',
+  COLUMN_ALIAS_USERNAME = 'aliasUsername',
+}
+
+export type PatchUserPayload = {
+  [PatchUserPayloadFields.COLUMN_FIRSTNAME]?: string;
+  [PatchUserPayloadFields.COLUMN_LASTNAME]?: string;
+  [PatchUserPayloadFields.COLUMN_PHONE]?: string;
+  [PatchUserPayloadFields.COLUMN_TITLE]?: string;
+  [PatchUserPayloadFields.COLUMN_ORGANIZATION_UNIT_REF]?: string;
+  [PatchUserPayloadFields.COLUMN_ACTION]?: string;
+  [PatchUserPayloadFields.COLUMN_ALIAS_USERNAME]?: string;
+};
+
 export class PartnerClient extends AbstractRestfulClient {
   protected basePath = '/partners';
 
@@ -10,5 +42,111 @@ export class PartnerClient extends AbstractRestfulClient {
     this.path = `/${partnerReference}`;
 
     return this.delete(parameters);
+  }
+
+  public async patchUser(
+    partnerReference: string,
+    userReference: string,
+    payload: PatchUserPayload,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    this.path = `/${partnerReference}/users/${userReference}`;
+
+    return await this.patch(payload, parameters);
+  }
+
+  public async lockUser(
+    partnerReference: string,
+    userReference: string,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    return await this.patchUser(
+      partnerReference,
+      userReference,
+      { action: UpdateUserActionEnum.LOCK },
+      parameters,
+    );
+  }
+
+  public async unlockUser(
+    partnerReference: string,
+    userReference: string,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    return await this.patchUser(
+      partnerReference,
+      userReference,
+      { action: UpdateUserActionEnum.UNLOCK },
+      parameters,
+    );
+  }
+
+  public async allowDirectLoginUser(
+    partnerReference: string,
+    userReference: string,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    return await this.patchUser(
+      partnerReference,
+      userReference,
+      { action: UpdateUserActionEnum.ALLOW_DIRECT_LOGIN },
+      parameters,
+    );
+  }
+
+  public async blockDirectLoginUser(
+    partnerReference: string,
+    userReference: string,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    return await this.patchUser(
+      partnerReference,
+      userReference,
+      { action: UpdateUserActionEnum.BLOCK_DIRECT_LOGIN },
+      parameters,
+    );
+  }
+
+  public async disableMfaUser(
+    partnerReference: string,
+    userReference: string,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    return await this.patchUser(
+      partnerReference,
+      userReference,
+      { action: UpdateUserActionEnum.DISABLE_MFA },
+      parameters,
+    );
+  }
+
+  public async disableAliasUser(
+    partnerReference: string,
+    userReference: string,
+    aliasUsername: string,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    return await this.patchUser(
+      partnerReference,
+      userReference,
+      {
+        action: UpdateUserActionEnum.DISABLE_ALIAS,
+        aliasUsername,
+      },
+      parameters,
+    );
+  }
+
+  public async unlockInsecureLoginUser(
+    partnerReference: string,
+    userReference: string,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    return await this.patchUser(
+      partnerReference,
+      userReference,
+      { action: UpdateUserActionEnum.UNLOCK_INSECURE_LOGIN },
+      parameters,
+    );
   }
 }
