a7ea3e1a5c0dbf1148d261f247f128c0d1a2c413
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2024-05-15 20:14:01 +0200

    patners patch user

--- src/partner/partnerClient.ts
+++ src/partner/partnerClient.ts
@@ -1,5 +1,26 @@
 import { AbstractRestfulClient, Parameters } from '../abstractRestfulClient';
 
+export enum PatchUserPayloadFields {
+  COLUMN_FIRSTNAME = 'firstname',
+  COLUMN_LASTNAME = 'lastname',
+  COLUMN_PHONE = 'phone',
+  COLUMN_TITLE = 'title',
+  COLUMN_IS_LOCKED = 'isLocked',
+  COLUMN_ORGANIZATION_UNIT_REF = 'organizationUnitRef',
+}
+
+export type PatchUserPayload = {
+  [PatchUserPayloadFields.COLUMN_FIRSTNAME]?: string;
+  [PatchUserPayloadFields.COLUMN_LASTNAME]?: string;
+  [PatchUserPayloadFields.COLUMN_PHONE]?: string;
+  [PatchUserPayloadFields.COLUMN_TITLE]?: string;
+  [PatchUserPayloadFields.COLUMN_ORGANIZATION_UNIT_REF]?: string;
+};
+
+export type PatchLockUserPayload = {
+  [PatchUserPayloadFields.COLUMN_IS_LOCKED]?: boolean;
+};
+
 export class PartnerClient extends AbstractRestfulClient {
   protected basePath = '/partners';
 
@@ -10,5 +31,44 @@ export class PartnerClient extends AbstractRestfulClient {
     this.path = `/${partnerReference}`;
 
     return this.delete(parameters);
+  }
+
+  public async patchUser(
+    partnerReference: string,
+    userReference: string,
+    payload: PatchUserPayload,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    this.path = `/${partnerReference}/users/${userReference}`;
+
+    return await this.patch(payload, parameters);
+  }
+
+  public async lockUser(
+    partnerReference: string,
+    userReference: string,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    this.path = `/${partnerReference}/users/${userReference}`;
+
+    const payload: PatchLockUserPayload = {
+      [PatchUserPayloadFields.COLUMN_IS_LOCKED]: true,
+    };
+
+    return await this.patch(payload, parameters);
+  }
+
+  public async unlockUser(
+    partnerReference: string,
+    userReference: string,
+    parameters: Parameters = {},
+  ): Promise<void> {
+    this.path = `/${partnerReference}/users/${userReference}`;
+
+    const payload: PatchLockUserPayload = {
+      [PatchUserPayloadFields.COLUMN_IS_LOCKED]: false,
+    };
+
+    return await this.patch(payload, parameters);
   }
 }
