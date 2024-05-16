8b8c5d0444540d5e7978a181d556e7abd8093361
Vincent GJ <vincent.grandjean-joyeux1@arrow.com>
2024-05-16 17:45:53 +0200

    add partner register

---
+++ src/partner/entities/company/company.ts
@@ -0,0 +1,137 @@
+import { AbstractEntity } from '../../../abstractEntity';
+import { Address, AddressType } from './address/address';
+
+export enum PartnerCompanyFields {
+  COLUMN_REFERENCE = 'reference',
+  COLUMN_NAME = 'name',
+  COLUMN_ACRONYM = 'acronym',
+  COLUMN_INTERNAL_REFERENCE = 'internalReference',
+  COLUMN_PHONE = 'phone',
+  COLUMN_NUMBER_OF_EMPLOYEE = 'numberOfEmployee',
+  COLUMN_CORPORATE_EMAIL = 'corporateEmail',
+  COLUMN_WEBSITE = 'website',
+  COLUMN_VAT_NUMBER = 'vatNumber',
+  COLUMN_REGISTRATION_NUMBER = 'registrationNumber',
+  COLUMN_ADDRESS = 'address',
+  COLUMN_BILLING_ADDRESS = 'billingAddress',
+}
+
+export type PartnerCompanyType = {
+  reference: string;
+  name: string;
+  acronym: string;
+  internalReference: string;
+  phone: string;
+  numberOfEmployee: string;
+  corporateEmail: string;
+  website: string;
+  vatNumber: string;
+  registrationNumber: string;
+  address: AddressType;
+  billingAddress: AddressType;
+};
+
+export class PartnerCompany extends AbstractEntity<PartnerCompanyType> {
+  readonly #reference: string;
+  readonly #name: string;
+  readonly #acronym: string;
+  readonly #internalReference: string;
+  readonly #phone: string;
+  readonly #numberOfEmployee: string;
+  readonly #corporateEmail: string;
+  readonly #website: string;
+  readonly #vatNumber: string;
+  readonly #registrationNumber: string;
+  readonly #address: Address;
+  readonly #billingAddress: Address;
+
+  public constructor(companyData: PartnerCompanyType) {
+    super(companyData);
+    this.#reference = companyData[PartnerCompanyFields.COLUMN_REFERENCE];
+    this.#name = companyData[PartnerCompanyFields.COLUMN_NAME];
+    this.#acronym = companyData[PartnerCompanyFields.COLUMN_ACRONYM];
+    this.#internalReference =
+      companyData[PartnerCompanyFields.COLUMN_INTERNAL_REFERENCE];
+    this.#phone = companyData[PartnerCompanyFields.COLUMN_PHONE];
+    this.#numberOfEmployee =
+      companyData[PartnerCompanyFields.COLUMN_NUMBER_OF_EMPLOYEE];
+    this.#corporateEmail =
+      companyData[PartnerCompanyFields.COLUMN_CORPORATE_EMAIL];
+    this.#website = companyData[PartnerCompanyFields.COLUMN_WEBSITE];
+    this.#vatNumber = companyData[PartnerCompanyFields.COLUMN_VAT_NUMBER];
+    this.#registrationNumber =
+      companyData[PartnerCompanyFields.COLUMN_REGISTRATION_NUMBER];
+    this.#address = new Address(
+      companyData[PartnerCompanyFields.COLUMN_ADDRESS],
+    );
+    this.#billingAddress = new Address(
+      companyData[PartnerCompanyFields.COLUMN_BILLING_ADDRESS],
+    );
+  }
+
+  get reference(): string {
+    return this.#reference;
+  }
+
+  get name(): string {
+    return this.#name;
+  }
+
+  get acronym(): string {
+    return this.#acronym;
+  }
+
+  get internalReference(): string {
+    return this.#internalReference;
+  }
+
+  get phone(): string {
+    return this.#phone;
+  }
+
+  get numberOfEmployee(): string {
+    return this.#numberOfEmployee;
+  }
+
+  get corporateEmail(): string {
+    return this.#corporateEmail;
+  }
+
+  get website(): string {
+    return this.#website;
+  }
+
+  get vatNumber(): string {
+    return this.#vatNumber;
+  }
+
+  get registrationNumber(): string {
+    return this.#registrationNumber;
+  }
+
+  get address(): Address {
+    return this.#address;
+  }
+
+  get billingAddress(): Address {
+    return this.#billingAddress;
+  }
+
+  public toJSON(): PartnerCompanyType {
+    return {
+      [PartnerCompanyFields.COLUMN_REFERENCE]: this.reference,
+      [PartnerCompanyFields.COLUMN_NAME]: this.name,
+      [PartnerCompanyFields.COLUMN_ACRONYM]: this.acronym,
+      [PartnerCompanyFields.COLUMN_INTERNAL_REFERENCE]: this.internalReference,
+      [PartnerCompanyFields.COLUMN_PHONE]: this.phone,
+      [PartnerCompanyFields.COLUMN_NUMBER_OF_EMPLOYEE]: this.numberOfEmployee,
+      [PartnerCompanyFields.COLUMN_CORPORATE_EMAIL]: this.corporateEmail,
+      [PartnerCompanyFields.COLUMN_WEBSITE]: this.website,
+      [PartnerCompanyFields.COLUMN_VAT_NUMBER]: this.vatNumber,
+      [PartnerCompanyFields.COLUMN_REGISTRATION_NUMBER]: this
+        .registrationNumber,
+      [PartnerCompanyFields.COLUMN_ADDRESS]: this.address.toJSON(),
+      [PartnerCompanyFields.COLUMN_BILLING_ADDRESS]: this.billingAddress.toJSON(),
+    };
+  }
+}
--- src/partner/partnerClient.ts
+++ src/partner/partnerClient.ts
@@ -1,4 +1,100 @@
 import { AbstractRestfulClient, Parameters } from '../abstractRestfulClient';
+import { GetResult } from '../getResult';
+import { DataPartner } from './entities/dataPartner';
+
+export enum PartnerFields {
+  REGISTRATION_TYPE = 'registrationType',
+  USER = 'user',
+  CONTACT = 'contact',
+  COMPANY = 'company',
+  RECAPTCHA_TOKEN = 'recaptchaToken',
+}
+
+export enum PartnerUserPayloadFields {
+  LOGIN = 'login',
+  PASSWORD = 'password',
+  GLOBAL_ID = 'globalID',
+}
+
+export enum PartnerContactPayloadFields {
+  TITLE = 'title',
+  FIRST_NAME = 'firstName',
+  LAST_NAME = 'lastName',
+  PHONE = 'phone',
+  EMAIL = 'email',
+  TYPE = 'type',
+  //   COUNTRY = 'country',
+}
+
+export enum PartnerAddressFields {
+  LINE1 = 'line1',
+  LINE2 = 'line2',
+  CITY = 'city',
+  STATE = 'state',
+  ZIP = 'zip',
+  COUNTRY_CODE = 'countryCode',
+}
+
+export enum PartnerCompanyPayloadFields {
+  ACRONYM = 'acronym',
+  INTERNAL_REFERENCE = 'internalReference',
+  NAME = 'name',
+  PHONE = 'phone',
+  NUMBER_OF_EMPLOYEE = 'numberOfEmployee',
+  CORPORATE_EMAIL = 'corporateEmail',
+  CORPORATE_WEBSITE = 'corporateWebsite',
+  ADDRESS = 'address',
+  BILLING_ADDRESS = 'billingAddress',
+  REGISTRATION_NUMBER = 'registrationNumber',
+  VAT_NUMBER = 'vatNumber',
+}
+
+export type PartnerUserPayload = {
+  [PartnerUserPayloadFields.LOGIN]?: string;
+  [PartnerUserPayloadFields.PASSWORD]: string;
+  [PartnerUserPayloadFields.GLOBAL_ID]?: string;
+};
+
+export type PartnerContactPayload = {
+  [PartnerContactPayloadFields.TITLE]: string;
+  [PartnerContactPayloadFields.FIRST_NAME]: string;
+  [PartnerContactPayloadFields.LAST_NAME]: string;
+  [PartnerContactPayloadFields.PHONE]: string;
+  [PartnerContactPayloadFields.EMAIL]: string;
+  [PartnerContactPayloadFields.TYPE]: string;
+  //   [PartnerContactPayloadFields.COUNTRY]: string;
+};
+
+export type PartnerAddress = {
+  [PartnerAddressFields.LINE1]: string;
+  [PartnerAddressFields.LINE2]: string;
+  [PartnerAddressFields.CITY]: string;
+  [PartnerAddressFields.STATE]: string;
+  [PartnerAddressFields.ZIP]: string;
+  [PartnerAddressFields.COUNTRY_CODE]: string;
+};
+
+export type PartnerCompanyPayload = {
+  [PartnerCompanyPayloadFields.ACRONYM]: string;
+  [PartnerCompanyPayloadFields.INTERNAL_REFERENCE]: string;
+  [PartnerCompanyPayloadFields.NAME]: string;
+  [PartnerCompanyPayloadFields.PHONE]: string;
+  [PartnerCompanyPayloadFields.NUMBER_OF_EMPLOYEE]?: number;
+  [PartnerCompanyPayloadFields.CORPORATE_EMAIL]: string;
+  [PartnerCompanyPayloadFields.CORPORATE_WEBSITE]: string;
+  [PartnerCompanyPayloadFields.ADDRESS]: PartnerAddress;
+  [PartnerCompanyPayloadFields.BILLING_ADDRESS]?: PartnerAddress;
+  [PartnerCompanyPayloadFields.REGISTRATION_NUMBER]: string;
+  [PartnerCompanyPayloadFields.VAT_NUMBER]: string;
+};
+
+export type PostPartnerPayload = {
+  [PartnerFields.REGISTRATION_TYPE]: string;
+  [PartnerFields.USER]: PartnerUserPayload;
+  [PartnerFields.CONTACT]: PartnerContactPayload;
+  [PartnerFields.COMPANY]?: PartnerCompanyPayload;
+  [PartnerFields.RECAPTCHA_TOKEN]: string;
+};
 
 export class PartnerClient extends AbstractRestfulClient {
   protected basePath = '/partners';
@@ -10,5 +106,14 @@ export class PartnerClient extends AbstractRestfulClient {
     this.path = `/${partnerReference}`;
 
     return this.delete(parameters);
+  }
+
+  public async postPartner(
+    payload: PostPartnerPayload,
+    parameters: Parameters = {},
+  ): Promise<GetResult<DataPartner>> {
+    this.path = `/v1/register`;
+
+    return new GetResult(DataPartner, await this.post(payload, parameters));
   }
 }
