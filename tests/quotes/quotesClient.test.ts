259b5d8755b3dc2359773877c618a9ef9f7c0ee2
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2024-06-26 12:08:04 +0200

    quotes request for ibm

--- tests/quotes/quotesClient.test.ts
+++ tests/quotes/quotesClient.test.ts
@@ -2,6 +2,7 @@ import { PublicApiClient } from '../../src';
 import { expect } from 'chai';
 import { Axios, AxiosResponse } from 'axios';
 import sinon from 'sinon';
+import nock from 'nock';
 
 const QUOTES_MOCK_URL = 'https://quotes.localhost';
 const QUOTE_REFERENCE = 'XSP999';
@@ -38,6 +39,65 @@ describe('QuotesClient', () => {
       const result = await client.deleteQuote(QUOTE_REFERENCE);
 
       expect(result).to.be.deep.equals(expectedResult.data);
+    });
+  });
+
+  describe('requestQuote', () => {
+    const quoteClient = new PublicApiClient()
+      .getQuotesClient()
+      .setUrl(QUOTES_MOCK_URL);
+    it('should call requestQuote method', async () => {
+      const PAYLOAD_RESPONSE = {
+        status: 200,
+        data: {
+          message: 'Your reference as been sent successfully.',
+        },
+      };
+
+      nock(QUOTES_MOCK_URL)
+        .post('/quotes/request')
+        .reply(200, PAYLOAD_RESPONSE);
+
+      const result = await quoteClient.requestQuote({
+        agreeToReceiveCommunications: true,
+        agreeSharingInformation: true,
+        company: 'The Company',
+        country: 'FR',
+        email: 'user@the-company.com',
+        firstName: 'TheFirstname',
+        lastName: 'TheLastname',
+        phoneNumber: '',
+        vendor: 'PALOALTO',
+      });
+
+      expect(nock.isDone()).to.be.true;
+      expect(result.data.toJSON()).to.be.eqls(PAYLOAD_RESPONSE.data);
+    });
+  });
+
+  describe('requestQuoteForIbm', () => {
+    const quoteClient = new PublicApiClient()
+      .getQuotesClient()
+      .setUrl(QUOTES_MOCK_URL);
+    it('should call requestQuoteForIbm method', async () => {
+      const PAYLOAD_RESPONSE = {
+        status: 200,
+        data: {
+          message:
+            'Your reference as been sent successfully to the vendor IBM.',
+        },
+      };
+
+      nock(QUOTES_MOCK_URL)
+        .post('/quotes/request/ibm')
+        .reply(200, PAYLOAD_RESPONSE);
+
+      const result = await quoteClient.requestQuoteForIbm({
+        reference: 'XSP123456',
+      });
+
+      expect(nock.isDone()).to.be.true;
+      expect(result.data.toJSON()).to.be.eqls(PAYLOAD_RESPONSE.data);
     });
   });
 });
