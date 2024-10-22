063cfc62c23e3b2fa69c94938c7c728b9a423bd2
JPB <jpb.blond@arrow.com>
2024-10-22 11:27:50 +0200

    add availableProgram schema

--- src/graphqlApi/types/graphqlApiQueries.ts
+++ src/graphqlApi/types/graphqlApiQueries.ts
@@ -6,7 +6,11 @@ import {
 import { ContinentType, CountryType } from './entities/country';
 import { LicenseBudgetType } from './entities/licenseBudget';
 import { PartnertagType } from './entities/partnertag';
-import { SubscribedProgramType } from './entities/program';
+import {
+  GraphqlApiProgramType,
+  ProgramSkuPackAvailabilityType,
+  SubscribedProgramType,
+} from './entities/program';
 import { QuoteType } from './entities/quote';
 import { SubscriptionType } from './entities/subscription';
 import { UserHistoryType, UserType } from './entities/user';
@@ -147,6 +151,8 @@ export enum SelectDataField {
   LICENSE_BUDGET = 'licenseBudget',
   PARTNER = 'partner',
   PARTNERTAG = 'partnertag',
+  PROGRAM = 'program',
+  PROGRAM_SKU_PACK_AVAILABILITY = 'programSkuPackAvailability',
   QUOTE = 'quote',
   SUBSCRIBED_PROGRAM = 'subscribedProgram',
   SUBSCRIPTION = 'subscription',
@@ -171,6 +177,8 @@ export type SelectAllResponseDataType = {
   [SelectDataField.LICENSE_BUDGET]?: LicenseBudgetType[];
   [SelectDataField.PARTNER]?: PartnerType[];
   [SelectDataField.PARTNERTAG]?: PartnertagType[];
+  [SelectDataField.PROGRAM]?: GraphqlApiProgramType[];
+  [SelectDataField.PROGRAM_SKU_PACK_AVAILABILITY]?: ProgramSkuPackAvailabilityType[];
   [SelectDataField.QUOTE]?: QuoteType[];
   [SelectDataField.SUBSCRIBED_PROGRAM]?: SubscribedProgramType[];
   [SelectDataField.SUBSCRIPTION]?: SubscriptionType[];
@@ -225,6 +233,8 @@ export type SelectOneResponseDataType = {
   [SelectDataField.LICENSE_BUDGET]?: LicenseBudgetType;
   [SelectDataField.PARTNER]?: PartnerType;
   [SelectDataField.PARTNERTAG]?: PartnertagType;
+  [SelectDataField.PROGRAM]?: GraphqlApiProgramType;
+  [SelectDataField.PROGRAM_SKU_PACK_AVAILABILITY]?: ProgramSkuPackAvailabilityType;
   [SelectDataField.QUOTE]?: QuoteType;
   [SelectDataField.SUBSCRIBED_PROGRAM]?: SubscribedProgramType;
   [SelectDataField.SUBSCRIPTION]?: SubscriptionType;
