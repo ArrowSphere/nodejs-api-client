6b011b03fd7a18f982f3d37a83b064e263f356bb
Maher Landolsi <maher.landolsi@arrow.com>
2025-01-15 15:43:51 +0100

    add missing campaign endpoints & add status to campaign

--- tests/campaign/campaignClient.test.ts
+++ tests/campaign/campaignClient.test.ts
@@ -21,8 +21,11 @@ import {
   GET_DETAILS_CAMPAIGN_V2_WITHOUT_FEATURE_ITEMS_OUTPUT,
   PAYLOAD_POST_CAMPAIGN_EMAIL,
   GET_DETAILS_CAMPAIGN_V2_MINIMAL_OUTPUT,
+  GET_ACTIVE_CAMPAIGN_OUTPUT_WITHOUT_BANNER,
+  GET_ACTIVE_CAMPAIGN_OUTPUT_WITHOUT_LANDING_PAGE,
 } from './mocks/campaign.mocks';
 import {
+  GET_CAMPAIGN_ASSETS_FORM_OUTPUT,
   GET_CAMPAIGN_ASSETS_OUTPUT,
   GET_CAMPAIGN_ASSETS_PARAMETERS,
 } from './mocks/campaignAssets.mock';
@@ -31,7 +34,14 @@ import { CampaignList } from '../../src/campaign/entities/v2/campaignList';
 export const CAMPAIGN_MOCK_URL = 'https://campaign.localhost';
 export const GET_ACTIVE_CAMPAIGN = new RegExp('/active.*');
 export const GET_CAMPAIGN_DETAILS = new RegExp('/campaigns.*');
+export const POST_DUPLICATE_CAMPAIGN = new RegExp('/campaigns/.*/duplicate');
+export const POST_CREATE_CAMPAIGN = new RegExp('/campaigns');
+export const DELETE_CAMPAIGN = new RegExp('/campaigns.*');
+export const PUT_CAMPAIGN = new RegExp('/campaigns.*');
+export const GET_LIST_CAMPAIGN = new RegExp('/campaigns');
 export const GET_CAMPAIGN_ASSETS = new RegExp('/.*./assets');
+export const GET_CAMPAIGN_ASSETS_FORM = new RegExp('/.*./assets/upload');
+export const DELETE_CAMPAIGN_ASSETS = new RegExp('/.*./assets/.*');
 export const POST_CAMPAIGN_EMAIL = new RegExp('/.*./notify');
 
 describe('CampaignClient', () => {
@@ -102,6 +112,30 @@ describe('CampaignClient', () => {
         GET_ACTIVE_CAMPAIGN_OUTPUT_WITHOUT_FOOTER_FEATURES,
       );
     });
+    it('Can handle a response without Banner', async () => {
+      nock(CAMPAIGN_MOCK_URL)
+        .get(GET_ACTIVE_CAMPAIGN)
+        .reply(200, GET_ACTIVE_CAMPAIGN_OUTPUT_WITHOUT_BANNER);
+
+      const response: GetResult<Campaign> = await client.getActiveCampaign();
+
+      expect(response).to.be.instanceof(GetResult);
+      expect(response.toJSON()).to.be.deep.equals(
+        GET_ACTIVE_CAMPAIGN_OUTPUT_WITHOUT_BANNER,
+      );
+    });
+    it('Can handle a response without landing page', async () => {
+      nock(CAMPAIGN_MOCK_URL)
+        .get(GET_ACTIVE_CAMPAIGN)
+        .reply(200, GET_ACTIVE_CAMPAIGN_OUTPUT_WITHOUT_LANDING_PAGE);
+
+      const response: GetResult<Campaign> = await client.getActiveCampaign();
+
+      expect(response).to.be.instanceof(GetResult);
+      expect(response.toJSON()).to.be.deep.equals(
+        GET_ACTIVE_CAMPAIGN_OUTPUT_WITHOUT_LANDING_PAGE,
+      );
+    });
   });
 
   describe('getCampaignAssets', () => {
@@ -130,6 +164,28 @@ describe('CampaignClient', () => {
 
       expect(response).to.be.instanceof(GetResult);
       expect(response.toJSON()).to.be.deep.equals(GET_CAMPAIGN_ASSETS_OUTPUT);
+    });
+    it('calls the get campaign assets form ', async () => {
+      nock(CAMPAIGN_MOCK_URL)
+        .get(GET_CAMPAIGN_ASSETS_FORM)
+        .reply(200, GET_CAMPAIGN_ASSETS_FORM_OUTPUT);
+
+      const response: GetResult<CampaignAssets> = await client.getCampaignUploadAssetsForm(
+        '0ce70536-a59d-4c21-b39d-272b034367fa',
+      );
+
+      expect(response).to.be.instanceof(GetResult);
+      expect(response.toJSON()).to.be.deep.equals(
+        GET_CAMPAIGN_ASSETS_FORM_OUTPUT,
+      );
+    });
+    it('calls the delete campaign assets form ', async () => {
+      nock(CAMPAIGN_MOCK_URL).delete(DELETE_CAMPAIGN_ASSETS).reply(200);
+
+      await client.deleteAssets(
+        '0ce70536-a59d-4c21-b39d-272b034367fa',
+        'af3ac6a6-12ab-4880-9dfe-52e734325444',
+      );
     });
   });
 
@@ -250,6 +306,57 @@ describe('CampaignClient', () => {
       expect(response.toJSON()).to.be.deep.equals(
         GET_DETAILS_CAMPAIGN_V2_WITHOUT_FEATURE_ITEMS_OUTPUT,
       );
+    });
+    it('calls the duplicate campaign', async () => {
+      nock(CAMPAIGN_MOCK_URL)
+        .post(POST_DUPLICATE_CAMPAIGN)
+        .reply(200, GET_DETAILS_CAMPAIGN_V2_WITHOUT_FEATURE_ITEMS_OUTPUT);
+
+      const response: GetResult<CampaignV2> = await client.duplicateCampaign(
+        '0ce70536-a59d-4c21-b39d-272b034367fa',
+      );
+
+      expect(response).to.be.instanceof(GetResult);
+      expect(response.toJSON()).to.be.deep.equals(
+        GET_DETAILS_CAMPAIGN_V2_WITHOUT_FEATURE_ITEMS_OUTPUT,
+      );
+    });
+    it('calls list campaigns', async () => {
+      nock(CAMPAIGN_MOCK_URL)
+        .get(GET_LIST_CAMPAIGN)
+        .reply(200, GET_ACTIVE_CAMPAIGNS_V2_OUTPUT);
+
+      const response: GetResult<CampaignList> = await client.getCampaignList();
+
+      expect(response).to.be.instanceof(GetResult);
+      expect(response.toJSON()).to.be.deep.equals(
+        GET_ACTIVE_CAMPAIGNS_V2_OUTPUT,
+      );
+    });
+    it('calls create campaign', async () => {
+      nock(CAMPAIGN_MOCK_URL).post(POST_CREATE_CAMPAIGN).reply(200);
+
+      await client.createCampaign({
+        name: 'Campaign name',
+        startDate: '2021-07-13T08:36:54Z',
+        endDate: '2021-07-13T08:36:54Z',
+        status: 'active',
+      });
+    });
+    it('calls update campaign', async () => {
+      nock(CAMPAIGN_MOCK_URL).put(PUT_CAMPAIGN).reply(200);
+
+      await client.updateCampaign('0ce70536-a59d-4c21-b39d-272b034367fa', {
+        name: 'Campaign name',
+        startDate: '2021-07-13T08:36:54Z',
+        endDate: '2021-07-13T08:36:54Z',
+        status: 'active',
+      });
+    });
+    it('calls delete campaign', async () => {
+      nock(CAMPAIGN_MOCK_URL).delete(DELETE_CAMPAIGN).reply(200);
+
+      await client.deleteCampaign('0ce70536-a59d-4c21-b39d-272b034367fa');
     });
   });
 
