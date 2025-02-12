1d5a3f9cd41711571d0006c68db2c5e6324c8ed2
Maher Landolsi <maher.landolsi@arrow.com>
2025-02-12 14:41:33 +0100

    fix campaign aggregation schema

--- src/campaign/entities/campaign/aggregation/category.ts
+++ src/campaign/entities/campaign/aggregation/category.ts
@@ -7,21 +7,21 @@ export enum CategoryEnumFields {
 }
 
 export type CategoryType = {
-  [CategoryEnumFields.COLUMN_DATE_AGG]: DateAggType[];
+  [CategoryEnumFields.COLUMN_DATE_AGG]: DateAggType;
   [CategoryEnumFields.COLUMN_NAME]: string;
 };
 
 export class Category extends AbstractEntity<CategoryType> {
-  readonly #dateAgg: DateAgg[];
+  readonly #dateAgg: DateAgg;
   readonly #name: string;
 
   constructor(data: CategoryType) {
     super(data);
-    this.#dateAgg = data.dateAgg.map((dateAgg) => new DateAgg(dateAgg));
+    this.#dateAgg = new DateAgg(data.dateAgg);
     this.#name = data.name;
   }
 
-  get dateAgg(): DateAgg[] {
+  get dateAgg(): DateAgg {
     return this.#dateAgg;
   }
 
@@ -31,9 +31,7 @@ export class Category extends AbstractEntity<CategoryType> {
 
   public toJSON(): CategoryType {
     return {
-      [CategoryEnumFields.COLUMN_DATE_AGG]: this.dateAgg.map(
-        (dateAgg: DateAgg) => dateAgg.toJSON(),
-      ),
+      [CategoryEnumFields.COLUMN_DATE_AGG]: this.dateAgg.toJSON(),
       [CategoryEnumFields.COLUMN_NAME]: this.name,
     };
   }
