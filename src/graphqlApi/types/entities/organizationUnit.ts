b3a5c408670b6b58ffaad1c936536c5edf3a30ac
Bairing Djonkamla <bairing.djonkamla@arrow.com>
2025-02-26 17:59:52 +0100

    graphql-api update contact schema

--- src/graphqlApi/types/entities/organizationUnit.ts
+++ src/graphqlApi/types/entities/organizationUnit.ts
@@ -1,4 +1,11 @@
+import { EndCustomerType } from './company';
+import { ContactsType } from './contact';
+
 export type OrganizationUnitsType = {
   id?: number;
+  contacts?: ContactsType[];
+  endCustomer?: EndCustomerType;
+  endCustomersCount?: number;
   name?: string;
+  usersCounts?: number;
 };
