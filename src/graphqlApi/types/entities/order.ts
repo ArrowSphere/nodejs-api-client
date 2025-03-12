f93c407439e3faf07ffe85e8e618a30a262718e9
Bairing Djonkamla <bairing.djonkamla1@arrow.com>
2025-03-12 12:53:37 +0100

    graphql-api get order details

--- src/graphqlApi/types/entities/order.ts
+++ src/graphqlApi/types/entities/order.ts
@@ -1,14 +1,46 @@
+import { ArrowCompanyType, EndCustomerType, PartnerType } from './company';
+import { ContactsType } from './contact';
+import { GraphqlApiEavType } from './eav';
+import { GraphqlApiProgramType } from './program';
+import { ItemData, QuoteType } from './quote';
 import { SpecialPriceRateType } from './specialPriceRate';
 import { GraphqlApiUnitType } from './unit';
+import { UserType } from './user';
 
 export type OrdersType = {
   id?: number;
+  arrowCompany?: ArrowCompanyType;
+  arrowContact?: ContactsType;
+  commitmentAmountTotal?: number;
+  createdAt?: string;
+  eavs?: GraphqlApiEavType[];
+  endCustomer?: EndCustomerType;
+  endCustomerContact?: ContactsType;
+  endCustomerPoNumber?: string;
+  fulfilledAt?: string;
   items?: OrderItemsType[];
+  partner?: PartnerType;
+  partnerContact?: ContactsType;
+  partnerPoNumber?: string;
+  quote?: QuoteType;
+  reference?: string;
+  scheduledAt?: string;
+  status?: GraphqlApiOrderStatusType;
+  totalRecurringPrice?: number;
+  updatedAt?: string;
 };
 
 export type OrderItemsType = {
   id?: number;
+  itemData?: ItemData;
+  migratedFrom?: GraphqlApiOrderLinkType[];
+  migratedTo?: GraphqlApiOrderLinkType[];
+  name?: string;
+  order?: OrdersType;
   priceRates?: SpecialPriceRateType[];
+  program?: GraphqlApiProgramType;
+  reference?: string;
+  status?: GraphqlApiOrderStatusType;
 };
 
 export type GraphqlApiOrderSoftwareType = {
@@ -16,4 +48,24 @@ export type GraphqlApiOrderSoftwareType = {
   customerId?: number;
   totalAmount?: number;
   unit?: GraphqlApiUnitType;
+};
+
+export type GraphqlApiOrderStatusType = {
+  id?: number;
+  name?: string;
+};
+
+export type GraphqlApiOrderLinkType = {
+  from?: OrderItemsType;
+  migratedAt?: string;
+  to?: OrderItemsType;
+};
+
+export type GraphqlApiOrderHistoryType = {
+  id?: number;
+  action?: string;
+  createdAt?: string;
+  description?: string;
+  order?: OrdersType;
+  user?: UserType;
 };
