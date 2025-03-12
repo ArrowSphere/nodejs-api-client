5e2de095cd4bc7c235037d8b5c4532effec38167
Bairing Djonkamla <bairing.djonkamla1@arrow.com>
2025-03-12 12:53:37 +0100

    graphql-api get order details

--- src/graphqlApi/types/entities/order.ts
+++ src/graphqlApi/types/entities/order.ts
@@ -1,14 +1,42 @@
+import { ArrowCompanyType, EndCustomerType, PartnerType } from './company';
+import { ContactsType } from './contact';
+import { GraphqlApiEavType } from './eav';
+import { GraphqlApiProgramType } from './program';
+import { ItemData, QuoteType } from './quote';
 import { SpecialPriceRateType } from './specialPriceRate';
 import { GraphqlApiUnitType } from './unit';
 
 export type OrdersType = {
   id?: number;
+  arrowCompany?: ArrowCompanyType;
+  arrowContact?: ContactsType;
+  commitmentAmountTotal?: number;
+  createdAt?: string;
+  eavs?: GraphqlApiEavType[];
+  endCustomer?: EndCustomerType;
+  endCustomerContact?: ContactsType;
+  endCustomerPoNumber?: string;
+  fulfilledAt?: string;
   items?: OrderItemsType[];
+  partner?: PartnerType;
+  partnerContact?: ContactsType;
+  partnerPoNumber?: string;
+  quote?: QuoteType;
+  reference?: string;
+  scheludedAt?: string;
+  status?: GraphqlApiOrderStatusType;
+  totalRecurringPrice?: number;
+  updatedAt?: string;
 };
 
 export type OrderItemsType = {
   id?: number;
+  itemData?: ItemData;
+  name?: string;
   priceRates?: SpecialPriceRateType[];
+  program?: GraphqlApiProgramType;
+  reference?: string;
+  status?: GraphqlApiOrderStatusType;
 };
 
 export type GraphqlApiOrderSoftwareType = {
@@ -16,4 +44,9 @@ export type GraphqlApiOrderSoftwareType = {
   customerId?: number;
   totalAmount?: number;
   unit?: GraphqlApiUnitType;
+};
+
+export type GraphqlApiOrderStatusType = {
+  id?: number;
+  name?: string;
 };
