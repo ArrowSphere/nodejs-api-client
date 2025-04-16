efae7ccb865f4b2849770f346c5f7bacf565b7d2
Félicien DIANOUX <felicien.dianoux@arrow.com>
2025-04-16 11:52:25 +0200

    improve error from client

--- src/exception/notFoundException.ts
+++ src/exception/notFoundException.ts
@@ -1,10 +1,17 @@
 export class NotFoundException extends Error {
   public httpCode: number;
   public httpError: string;
+  public config?: Record<string, unknown>;
 
-  constructor(message: string, httpError = '', httpCode = 0) {
+  constructor(
+    message: string,
+    httpError = '',
+    httpCode = 0,
+    config: Record<string, unknown> = {},
+  ) {
     super(message);
     this.httpCode = httpCode;
     this.httpError = httpError;
+    this.config = config;
   }
 }
