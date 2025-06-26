a755960bc625729acd769a9551cf18a208054ec5
Nicolas Foiny <nicolas.foiny@arrow.com>
2025-06-26 14:39:14 +0200

    Enhance the campaign types

--- src/campaign/campaignClient.ts
+++ src/campaign/campaignClient.ts
@@ -18,49 +18,49 @@ export type PostEmailCampaignType = {
 };
 
 export enum CampaignInputFields {
-  COLUMN_NAME = 'name',
+  COLUMN_BANNER = 'banner',
   COLUMN_CATEGORY = 'category',
+  COLUMN_END_DATE = 'endDate',
   COLUMN_IS_ACTIVATED = 'isActivated',
-  COLUMN_STATUS = 'status',
+  COLUMN_LANDING_PAGE = 'landingPage',
+  COLUMN_NAME = 'name',
   COLUMN_RULES = 'rules',
   COLUMN_START_DATE = 'startDate',
-  COLUMN_END_DATE = 'endDate',
-  COLUMN_BANNER = 'banner',
-  COLUMN_LANDING_PAGE = 'landingPage',
+  COLUMN_STATUS = 'status',
 }
 
 export enum RulesInputFields {
-  COLUMN_LOCATIONS = 'locations',
-  COLUMN_ROLES = 'roles',
-  COLUMN_MARKETPLACES = 'marketplaces',
-  COLUMN_SUBSCRIPTIONS = 'subscriptions',
-  COLUMN_RESELLERS = 'resellers',
   COLUMN_END_CUSTOMERS = 'endCustomers',
+  COLUMN_LOCATIONS = 'locations',
+  COLUMN_MARKETPLACES = 'marketplaces',
+  COLUMN_RESELLERS = 'resellers',
+  COLUMN_ROLES = 'roles',
+  COLUMN_SUBSCRIPTIONS = 'subscriptions',
 }
 
 export type RulesInputType = {
-  [RulesInputFields.COLUMN_LOCATIONS]?: string[];
-  [RulesInputFields.COLUMN_ROLES]?: string[];
-  [RulesInputFields.COLUMN_MARKETPLACES]?: string[];
-  [RulesInputFields.COLUMN_SUBSCRIPTIONS]?: string[];
-  [RulesInputFields.COLUMN_RESELLERS]?: string[];
   [RulesInputFields.COLUMN_END_CUSTOMERS]?: string[];
+  [RulesInputFields.COLUMN_LOCATIONS]?: string[];
+  [RulesInputFields.COLUMN_MARKETPLACES]?: string[];
+  [RulesInputFields.COLUMN_RESELLERS]?: string[];
+  [RulesInputFields.COLUMN_ROLES]?: string[];
+  [RulesInputFields.COLUMN_SUBSCRIPTIONS]?: string[];
 };
 
 export enum BannerInputFields {
-  COLUMN_TYPE = 'type',
   COLUMN_BUTTON_PLACEMENT = 'buttonPlacement',
   COLUMN_BUTTON_TEXT = 'buttonText',
   COLUMN_TEXT = 'text',
   COLUMN_TEXT_COLOR = 'textColor',
+  COLUMN_TYPE = 'type',
 }
 
 export type BannerInputType = {
-  [BannerInputFields.COLUMN_TYPE]?: string;
   [BannerInputFields.COLUMN_BUTTON_PLACEMENT]?: string;
   [BannerInputFields.COLUMN_BUTTON_TEXT]?: string;
   [BannerInputFields.COLUMN_TEXT]?: string;
   [BannerInputFields.COLUMN_TEXT_COLOR]?: string;
+  [BannerInputFields.COLUMN_TYPE]?: string;
 };
 
 export enum LandingPageHeaderInputFields {
@@ -74,12 +74,13 @@ export enum LandingPageHeaderInputFields {
 export type LandingPageHeaderInputType = {
   [LandingPageHeaderInputFields.COLUMN_BACKGROUND_COLOR]?: string;
   [LandingPageHeaderInputFields.COLUMN_BASELINE]?: string;
-  [LandingPageHeaderInputFields.COLUMN_TEXT_COLOR]?: string;
   [LandingPageHeaderInputFields.COLUMN_CIRCLE_COLOR]?: string;
+  [LandingPageHeaderInputFields.COLUMN_TEXT_COLOR]?: string;
   [LandingPageHeaderInputFields.COLUMN_TITLE]?: string;
 };
 
 export enum LandingPageBodyInputFields {
+  COLUMN_BACKGROUND_COLOR = 'backgroundColor',
   COLUMN_BUTTON_TEXT = 'buttonText',
   COLUMN_CONTACT_EMAIL = 'contactEmail',
   COLUMN_DESCRIPTION = 'description',
@@ -89,6 +90,7 @@ export enum LandingPageBodyInputFields {
 }
 
 export type LandingPageBodyInputType = {
+  [LandingPageBodyInputFields.COLUMN_BACKGROUND_COLOR]?: string;
   [LandingPageBodyInputFields.COLUMN_BUTTON_TEXT]?: string;
   [LandingPageBodyInputFields.COLUMN_CONTACT_EMAIL]?: string;
   [LandingPageBodyInputFields.COLUMN_DESCRIPTION]?: string;
@@ -98,19 +100,79 @@ export type LandingPageBodyInputType = {
 };
 
 export enum LandingPageFooterInputFields {
-  COLUMN_TITLE = 'title',
   COLUMN_BACKGROUND_COLOR = 'backgroundColor',
   COLUMN_BUTTON_TEXT = 'buttonText',
   COLUMN_BUTTON_URL = 'buttonUrl',
+  COLUMN_FEATURE = 'feature',
+  COLUMN_MARKETING_FEATURE = 'marketingFeature',
   COLUMN_TEXT_COLOR = 'textColor',
+  COLUMN_TITLE = 'title',
 }
 
 export type LandingPageFooterInputType = {
-  [LandingPageFooterInputFields.COLUMN_TITLE]?: string;
   [LandingPageFooterInputFields.COLUMN_BACKGROUND_COLOR]?: string;
   [LandingPageFooterInputFields.COLUMN_BUTTON_TEXT]?: string;
   [LandingPageFooterInputFields.COLUMN_BUTTON_URL]?: string;
+  [LandingPageFooterInputFields.COLUMN_FEATURE]?: LandingPageFooterFeatureInputType[];
+  [LandingPageFooterInputFields.COLUMN_MARKETING_FEATURE]?: LandingPageFooterMarketingFeatureInputFields[];
   [LandingPageFooterInputFields.COLUMN_TEXT_COLOR]?: string;
+  [LandingPageFooterInputFields.COLUMN_TITLE]?: string;
+};
+
+export enum LandingPageFooterFeatureInputFields {
+  COLUMN_DESCRIPTION = 'description',
+  COLUMN_ITEMS = 'items',
+  COLUMN_TITLE = 'title',
+}
+
+export type LandingPageFooterFeatureInputType = {
+  [LandingPageFooterFeatureInputFields.COLUMN_DESCRIPTION]?: string;
+  [LandingPageFooterFeatureInputFields.COLUMN_ITEMS]?: LandingPageFooterFeatureItemInputType[];
+  [LandingPageFooterFeatureInputFields.COLUMN_TITLE]?: string;
+};
+
+export enum LandingPageFooterMarketingFeatureInputFields {
+  COLUMN_DESCRIPTION = 'description',
+  COLUMN_ITEMS = 'items',
+  COLUMN_TITLE = 'title',
+}
+
+export type LandingPageFooterMarketingFeatureInputType = {
+  [LandingPageFooterFeatureInputFields.COLUMN_DESCRIPTION]?: string;
+  [LandingPageFooterFeatureInputFields.COLUMN_ITEMS]?: LandingPageFooterMarketingFeatureItemInputType[];
+  [LandingPageFooterFeatureInputFields.COLUMN_TITLE]?: string;
+};
+
+export enum LandingPageFooterFeatureItemInputFields {
+  COLUMN_BUTTON_TEXT = 'buttonText',
+  COLUMN_BUTTON_URL = 'buttonUrl',
+  COLUMN_DESCRIPTION = 'description',
+  COLUMN_ICON = 'icon',
+  COLUMN_TITLE = 'title',
+}
+
+export type LandingPageFooterFeatureItemInputType = {
+  [LandingPageFooterFeatureItemInputFields.COLUMN_BUTTON_TEXT]?: string;
+  [LandingPageFooterFeatureItemInputFields.COLUMN_BUTTON_URL]?: string;
+  [LandingPageFooterFeatureItemInputFields.COLUMN_DESCRIPTION]?: string;
+  [LandingPageFooterFeatureItemInputFields.COLUMN_ICON]?: string;
+  [LandingPageFooterFeatureItemInputFields.COLUMN_TITLE]?: string;
+};
+
+export enum LandingPageFooterMarketingFeatureItemInputFields {
+  COLUMN_BUTTON_TEXT = 'buttonText',
+  COLUMN_BUTTON_URL = 'buttonUrl',
+  COLUMN_DESCRIPTION = 'description',
+  COLUMN_SIZE = 'size',
+  COLUMN_TITLE = 'title',
+}
+
+export type LandingPageFooterMarketingFeatureItemInputType = {
+  [LandingPageFooterMarketingFeatureItemInputFields.COLUMN_BUTTON_TEXT]?: string;
+  [LandingPageFooterMarketingFeatureItemInputFields.COLUMN_BUTTON_URL]?: string;
+  [LandingPageFooterMarketingFeatureItemInputFields.COLUMN_DESCRIPTION]?: string;
+  [LandingPageFooterMarketingFeatureItemInputFields.COLUMN_SIZE]?: string;
+  [LandingPageFooterMarketingFeatureItemInputFields.COLUMN_TITLE]?: string;
 };
 
 export enum LandingPageInputFields {
@@ -121,22 +183,22 @@ export enum LandingPageInputFields {
 }
 
 export type LandingPageInputType = {
-  [LandingPageInputFields.COLUMN_URL]?: string;
-  [LandingPageInputFields.COLUMN_HEADER]: LandingPageHeaderInputType;
   [LandingPageInputFields.COLUMN_BODY]: LandingPageBodyInputType;
   [LandingPageInputFields.COLUMN_FOOTER]?: LandingPageFooterInputType;
+  [LandingPageInputFields.COLUMN_HEADER]: LandingPageHeaderInputType;
+  [LandingPageInputFields.COLUMN_URL]?: string;
 };
 
 export type CampaignInputType = {
-  [CampaignInputFields.COLUMN_NAME]: string;
+  [CampaignInputFields.COLUMN_BANNER]?: BannerInputType;
   [CampaignInputFields.COLUMN_CATEGORY]?: string;
+  [CampaignInputFields.COLUMN_END_DATE]?: string;
   [CampaignInputFields.COLUMN_IS_ACTIVATED]?: boolean;
-  [CampaignInputFields.COLUMN_STATUS]?: string;
+  [CampaignInputFields.COLUMN_LANDING_PAGE]?: LandingPageInputType;
+  [CampaignInputFields.COLUMN_NAME]: string;
   [CampaignInputFields.COLUMN_RULES]?: RulesInputType;
   [CampaignInputFields.COLUMN_START_DATE]?: string;
-  [CampaignInputFields.COLUMN_END_DATE]?: string;
-  [CampaignInputFields.COLUMN_BANNER]?: BannerInputType;
-  [CampaignInputFields.COLUMN_LANDING_PAGE]?: LandingPageInputType;
+  [CampaignInputFields.COLUMN_STATUS]?: string;
 };
 
 export type PostEmailCampaignMetadataType = {
