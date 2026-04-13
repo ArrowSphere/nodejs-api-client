aefe7e97f24252a2f84e4ee4be7724d1d6691fda
JPB <jpb.blond@arrow.com>
2026-04-13 16:49:32 +0200

    add customFields in Late Renew request

--- src/licenses/entities/lateRenewalLicense/executeLateRenewPayload.ts
+++ src/licenses/entities/lateRenewalLicense/executeLateRenewPayload.ts
@@ -3,7 +3,13 @@ export type ExecuteLateRenewLicense = {
   licenseRef: string;
 };
 
+export type CustomField = {
+  label: string;
+  value: string;
+};
+
 export type ExecuteLateRenewPayload = {
   licenses: ExecuteLateRenewLicense[];
   billingComments: Record<string, string>;
+  customFields: CustomField[];
 };
