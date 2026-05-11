2202ced835d92a6eee285abadafabc912e7d05a3
Sumit Chinchane <sumit.chinchane@arrow.com>
2026-05-11 10:12:02 +0530

    Added fields displayName, level and priority to GraphqlApiSupportLevel

--- src/graphqlApi/types/entities/supportLevel.ts
+++ src/graphqlApi/types/entities/supportLevel.ts
@@ -2,9 +2,12 @@ import { GraphqlApiProgramType } from './program';
 
 export type GraphqlApiSupportLevel = {
   id?: number;
+  displayName?: boolean;
   isPremium?: boolean;
   isEndCustomer?: boolean;
+  level?: string;
   label?: string;
   skus?: string;
   program?: GraphqlApiProgramType;
+  priority?: number;
 };
