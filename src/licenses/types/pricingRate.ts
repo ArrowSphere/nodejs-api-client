167a26999e8a62c9e971df4179afb1c72be22b64
Magid soudeni <Magid.soudeni@arrow.com>
2026-08-13 11:27:02 +0200

    add endpoint for price rate history and scheduled rates

---
+++ src/licenses/types/pricingRate.ts
@@ -0,0 +1,23 @@
+export type GetLicensePricingRateParameters = {
+  continuationToken?: string;
+  perPage?: number;
+};
+
+export type LicensePricingRateItem = {
+  rateType: string;
+  value: string;
+  changeRequestDate: string;
+  effectiveDate: string;
+  source: string;
+};
+
+export type LicensePricingRatePagination = {
+  perPage: number;
+  continuationToken: string | null;
+};
+
+export type GetLicensePricingRateResponse = {
+  status: number;
+  data: LicensePricingRateItem[];
+  pagination: LicensePricingRatePagination;
+};
