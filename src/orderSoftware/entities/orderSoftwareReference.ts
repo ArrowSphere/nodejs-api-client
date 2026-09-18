008cdaacf2b3282eea905922f311ee7c8de6214d
arrow <sanjaykumar.das@arrow.com>
2026-09-18 12:54:07 +0530

    feat(orderSoftware): add sellingPrice and ratio fields

--- src/orderSoftware/entities/orderSoftwareReference.ts
+++ src/orderSoftware/entities/orderSoftwareReference.ts
@@ -3,21 +3,25 @@ import { AbstractEntity } from '../../abstractEntity';
 export enum OrderSoftwareReferenceFields {
   COLUMN_REFERENCE = 'reference',
   COLUMN_LINK = 'link',
+  COLUMN_RATIO = 'ratio',
 }
 
 export type OrderSoftwareReferenceType = {
   [OrderSoftwareReferenceFields.COLUMN_REFERENCE]: string;
   [OrderSoftwareReferenceFields.COLUMN_LINK]: string;
+  [OrderSoftwareReferenceFields.COLUMN_RATIO]?: number;
 };
 
 export class OrderSoftwareReference extends AbstractEntity<OrderSoftwareReferenceType> {
   readonly #reference: string;
   readonly #link: string;
+  readonly #ratio?: number;
 
   public constructor(reference: OrderSoftwareReferenceType) {
     super(reference);
     this.#reference = reference[OrderSoftwareReferenceFields.COLUMN_REFERENCE];
     this.#link = reference[OrderSoftwareReferenceFields.COLUMN_LINK];
+    this.#ratio = reference[OrderSoftwareReferenceFields.COLUMN_RATIO];
   }
 
   get reference() {
@@ -28,10 +32,15 @@ export class OrderSoftwareReference extends AbstractEntity<OrderSoftwareReferenc
     return this.#link;
   }
 
+  get ratio(): number | undefined {
+    return this.#ratio;
+  }
+
   public toJSON(): OrderSoftwareReferenceType {
     return {
       [OrderSoftwareReferenceFields.COLUMN_REFERENCE]: this.reference,
       [OrderSoftwareReferenceFields.COLUMN_LINK]: this.link,
+      [OrderSoftwareReferenceFields.COLUMN_RATIO]: this.ratio,
     };
   }
 }
